a1e9a8a747560424896d32cb3935e9e48bdef04b
John Gilbert <johngilb@gmail.com>
2018-01-11 18:31:01 -0800

    Use the competition module.

--- CompetitionTemplate/src/main/java/competition/Robot.java
+++ CompetitionTemplate/src/main/java/competition/Robot.java
@@ -4,9 +4,18 @@ package competition;
 import competition.operator_interface.OperatorCommandMap;
 import competition.subsystems.SubsystemDefaultCommandMap;
 import xbot.common.command.BaseRobot;
+import xbot.common.properties.DoubleProperty;
+import xbot.common.properties.XPropertyManager;
 
 public class Robot extends BaseRobot {
 
+	DoubleProperty example;
+	
+	@Override
+    protected void setupInjectionModule() {
+        this.injectionModule = new CompetitionModule();
+    }
+	
     @Override
     protected void initializeSystems() {
         super.initializeSystems();
