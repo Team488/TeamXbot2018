fd69f3c3c3116f86942be11a46772a38e85300b2
veronayue <veronayue@gmail.com>
2018-01-11 20:51:55 -0800

    brainstorm the basic  skeletonClimbSubsystem

--- CompetitionTemplate/src/main/java/competition/subsystems/climb/skeletonClimbSubsystem.java
+++ CompetitionTemplate/src/main/java/competition/subsystems/climb/skeletonClimbSubsystem.java
@@ -1,5 +1,50 @@
 package competition.subsystems.climb;
 
-public class skeletonClimbSubsystem {
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
 
+import xbot.common.command.BaseSubsystem;
+
+@Singleton
+public class skeletonClimbSubsystem extends BaseSubsystem{
+	
+	 @Inject
+	public skeletonClimbSubsystem() {
+		// TODO Auto-generated constructor stub
+	
+		 }
+	 
+	 /**
+	  * The arms will extend longer
+	 **/
+		  public void extend(){
+	}
+	
+	/**
+	 * The arms will withdraw 
+	 */
+		  public void withdraw(){
+			  
+	}
+		  public void stop(){
+			  
+	}
+	/**
+	 * This boolean will determine if the arms extend close to the longest length
+	 * @return
+	 */
+		  boolean isCloseLongest(){
+			  return false;
+			  
+	}
+	/**
+	 * if the arms withdraw and it's close to the shortest length
+	 * @return
+	 */
+		  boolean isCloseShortest(){
+			  return false;
+			  
+	}
+		  public void de
+		  
 }
