75825d06b718f102f1aa39864dde2e2d229612c4
Gabriel Young <peregrineautumn@gmail.com>
2018-01-13 13:57:32 -0800

    ADd a speed property, remove unnecessary speed methods

--- CompetitionTemplate/src/main/java/competition/subsystems/lean/LeanSubsystem.java
+++ CompetitionTemplate/src/main/java/competition/subsystems/lean/LeanSubsystem.java
@@ -1,7 +1,64 @@
 package competition.subsystems.lean;
 
+import com.google.inject.Inject;
+
 import xbot.common.command.BaseSubsystem;
+import xbot.common.properties.XPropertyManager;
 
-public class LeanSubsystem extends BaseSubsystem
+public class LeanSubsystem extends BaseSubsystem {
 
+	DoubleProperty leanSpeed;
+	
+	@Inject
+	public LeanSubsystem(XPropertyManager propMan) {
+		
+	}
+	
+	/**
+	 * makes the climb arm lean left
+	 */
+	public void leanLeft() {
+		
+	}
+	
+	/**
+	 * makes the climb arm lean right
+	 */
+	public void leanRight() {
+	
+	}
+	
+	/**
+	 * moves the arm to center position
+	 */
+	public void center() {
+		
+	}
+	
+	/**
+	 * stops the climb arm from leaning left
+	 */
+	public void stopLean() {
+		
+	}
+	/**
+	 * sensor to determine if the arm has hit the bar
+	 */
+	public boolean HitBar() {
+		return false;
+	}
+	
+	/**
+	 * slows down the speed of the leaning arm
+	 */
+	public void slowLean() {
+		
+	}
+	
+	/**
+	 * speeds up the speed of the leaning arm
+	 */
+	public void fastLean() {
+		
+	}
 }
