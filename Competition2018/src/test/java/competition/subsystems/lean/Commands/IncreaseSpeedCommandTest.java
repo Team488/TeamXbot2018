61d615a9fb5aa316dd077cce4a53c671a7b82ad5
Mr-Recalculate <peregrineautumn@gmail.com>
2018-01-20 13:04:16 -0800

    Feature/skeleton climb lean subsystyem (#17)
    * Work thus far
    
    * Move to better folder
    
    * finished tests for ClimberLean Subsystem. moving on to tests for deploy subsystem
    
    * managed to not select this file for my last push
    
    * Added Tests for Climber Deploy Commands
    
    * started to fix this thing
    
    * addressed as many comments I could
    
    * I fixed the Tests, commands and subsystems.
    
    * Fixed second round of comments

---
+++ Competition2018/src/test/java/competition/subsystems/lean/Commands/IncreaseSpeedCommandTest.java
@@ -0,0 +1,44 @@
+package competition.subsystems.lean.Commands;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import competition.subsystems.lean.LeanSubsystem;
+import competition.subsystems.lean.commands.IncreaseSpeedCommand;
+import xbot.common.controls.actuators.mock_adapters.MockCANTalon;
+import xbot.common.injection.BaseWPITest;
+
+public class IncreaseSpeedCommandTest extends BaseWPITest{
+	
+	LeanSubsystem lean;
+	IncreaseSpeedCommand command;
+	
+	@Override
+	public void setUp() {
+		super.setUp();
+		
+		lean = injector.getInstance(LeanSubsystem.class);
+		command = injector.getInstance(IncreaseSpeedCommand.class);
+		
+		lean.temporaryHack();
+	}
+	
+	@Test
+	public void simpleTest() {
+		command.initialize();
+		command.execute();
+	}
+	
+	@Test
+	public void checkIncreaseSpeed() {
+		lean.decreaseSpeed();
+		lean.leanRight();
+		assertEquals(-0.1, lean.motor.getMotorOutputPercent(), 0.001);
+		command.initialize();
+		command.execute();
+		lean.leanRight();
+		assertEquals(-0.4, lean.motor.getMotorOutputPercent(), 0.001);
+	}
+}
