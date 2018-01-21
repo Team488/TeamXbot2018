d4ae02f473ab0ec719636b111aff06a5a66c1e02
John Gilbert <johngilb@gmail.com>
2018-01-20 16:24:18 -0800

    Started work on calibration

--- Competition2018/src/main/java/competition/subsystems/elevator/ElevatorSubsystem.java
+++ Competition2018/src/main/java/competition/subsystems/elevator/ElevatorSubsystem.java
@@ -5,7 +5,13 @@ import com.google.inject.Singleton;
 
 import xbot.common.command.BaseSubsystem;
 import xbot.common.controls.actuators.XCANTalon;
+import xbot.common.controls.sensors.XDigitalInput;
 import xbot.common.injection.wpi_factories.CommonLibFactory;
+import xbot.common.logic.Latch;
+import xbot.common.logic.Latch.EdgeType;
+import xbot.common.math.MathUtils;
+import xbot.common.properties.DoubleProperty;
+import xbot.common.properties.XPropertyManager;
 
 
 @Singleton
@@ -13,27 +19,60 @@ public class ElevatorSubsystem extends BaseSubsystem {
 	
 	double defaultElevatorPower;
 	CommonLibFactory clf;
+	final DoubleProperty elevatorPower;
+	final DoubleProperty elevatorTicksPerInch;
+	
+	/**
+	 * If our elevator is uncalibrated, we don't allow large power inputs.
+	 */
+	final DoubleProperty calibrationPower;
+	
+	private boolean _isCalibrated;
+	private double calibrationOffset;
+	private Latch calibrationLatch;
 	
 	double height;
 	double maxHeight;
 	double minHeight;
 	
 	public XCANTalon motor;
+	public XDigitalInput calibrationSensor;
 	
 	@Inject
-	public ElevatorSubsystem(CommonLibFactory clf) {
+	public ElevatorSubsystem(CommonLibFactory clf, XPropertyManager propMan) {
 		this.clf = clf;
+		elevatorPower = propMan.createPersistentProperty("ElevatorPower", 0.4);
+		elevatorTicksPerInch = propMan.createPersistentProperty("ElevatorTicksPerInch", 100);
+		calibrationPower = propMan.createPersistentProperty("ElevatorCalibrationPower", 0.2);
+		calibrationOffset = 0;
 	}
 	
 	public void temporaryHack() {
 		motor = clf.createCANTalon(40);
+		calibrationSensor = clf.createDigitalInput(1);
+		
+		calibrationLatch = new Latch(false, EdgeType.RisingEdge);
+		//calibrationLatch.addEdgeCallback(callback);
+	}
+	
+	public void setPower(double power) {
+	    
+	    
+	    
+	    if (!_isCalibrated) {
+	        MathUtils.constrainDouble(power, -calibrationPower.get(), calibrationPower.get());
+	        
+	        _isCalibrated = calibrationSensor.get();
+	    }
+	    
+	    motor.simpleSet(power);
 	}
 
 	/**
 	 * Raises the elevator. Power is controlled by a property.
 	 */
 	public void rise(){
-		motor.simpleSet(0.4);
+	    setPower(elevatorPower.get());
 	}
 	
 	
@@ -41,15 +80,28 @@ public class ElevatorSubsystem extends BaseSubsystem {
 	 * Lower the elevator. Power is controlled by a property.
 	 */
 	public void lower(){
-		motor.simpleSet(-0.4);
+	    setPower(-elevatorPower.get());
 	}
 	
 	public void stop(){
-		motor.simpleSet(0);
+	    setPower(0);
 	}
 	
 	public double currentHeight() {
-		return height;
+		return ticksToInches(motor.getSelectedSensorPosition(0));
+	}
+	
+	private double ticksToInches(double ticks) { 
+	    double tpi = elevatorTicksPerInch.get();
+	    if (tpi == 0) {
+	        return 0;
+	    }
+	    
+	    return (ticks - calibrationOffset) / tpi;
+	}
+	
+	public boolean isCalibrated() {
+	    return _isCalibrated;
 	}
 	
 	/**
@@ -77,19 +129,6 @@ public class ElevatorSubsystem extends BaseSubsystem {
 		}
 		
 		return false;
-	}
-
-	public void moveToMaxHeight() {
-		motor.simpleSet(0.4);
-	}
-	
-	public void moveToMinHeight() {
-		motor.simpleSet(-0.4);
-	}
-	
-	public void setPower(double power) {
-		motor.simpleSet(power);
-	}
-	
+	}	
 }
 	
