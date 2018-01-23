875d1a2f2da4c1612bd95398905980762d9e36ee
johnli01 <30376878+johnli01@users.noreply.github.com>
2018-01-23 11:21:27 -0800

    Feature/elevator simple command (#14)
    * Elevator Commands
    
    * ElevatorCommand
    
    * WIP Cleaning Elevator Commands
    
    * Test Works for MovingUp
    
    * Basic CommandTest Works
    
    * Current work thus far
    
    * use BaseCompetitionTest
    
    * Started work on calibration
    
    * Finish up simple calibration and add some tests

--- Competition2018/src/main/java/competition/subsystems/elevator/ElevatorSubsystem.java
+++ Competition2018/src/main/java/competition/subsystems/elevator/ElevatorSubsystem.java
@@ -1,11 +1,16 @@
 package competition.subsystems.elevator;
 
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 
 import xbot.common.command.BaseSubsystem;
 import xbot.common.controls.actuators.XCANTalon;
+import xbot.common.controls.sensors.XDigitalInput;
 import xbot.common.injection.wpi_factories.CommonLibFactory;
+import xbot.common.logic.Latch;
+import xbot.common.logic.Latch.EdgeType;
+import xbot.common.math.MathUtils;
 import xbot.common.properties.DoubleProperty;
 import xbot.common.properties.XPropertyManager;
 
@@ -14,41 +19,138 @@ import xbot.common.properties.XPropertyManager;
 public class ElevatorSubsystem extends BaseSubsystem {
 	
 	double defaultElevatorPower;
-	public XCANTalon motor;
 	CommonLibFactory clf;
 	final DoubleProperty elevatorPower;
+	final DoubleProperty elevatorTicksPerInch;
+	
+	/**
+	 * If our elevator is uncalibrated, we don't allow large power inputs.
+	 */
+	final DoubleProperty calibrationPower;
+	
+	private boolean _isCalibrated;
+	private double calibrationOffset;
+	private final Latch calibrationLatch;
+	
+	final DoubleProperty maxHeightInInches;
+	final DoubleProperty minHeightInInches;
+	
+	public XCANTalon motor;
+	public XDigitalInput calibrationSensor;
 	
 	@Inject
 	public ElevatorSubsystem(CommonLibFactory clf, XPropertyManager propMan) {
 		this.clf = clf;
 		elevatorPower = propMan.createPersistentProperty("ElevatorPower", 0.4);
+		elevatorTicksPerInch = propMan.createPersistentProperty("ElevatorTicksPerInch", 100);
+		calibrationPower = propMan.createPersistentProperty("ElevatorCalibrationPower", 0.2);
+		maxHeightInInches = propMan.createPersistentProperty("Elevator Max HeightInInches", 80);
+		minHeightInInches = propMan.createPersistentProperty("Elevator Min HeightInInches", 3);		
+		
+		calibrationOffset = 0;
+		
+		calibrationLatch = new Latch(false, EdgeType.RisingEdge, edge -> {
+		    if (edge == EdgeType.RisingEdge) {
+		        calibrate();
+		    }
+		});
 	}
 	
 	public void temporaryHack() {
-		motor = clf.createCANTalon(41);
+		motor = clf.createCANTalon(40);
+		motor.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder, 0, 0);
+		calibrationSensor = clf.createDigitalInput(1);
+	}
+	
+	private void calibrate() {
+	    calibrationOffset = motor.getSelectedSensorPosition(0);
+	    _isCalibrated = true;
+	}
+	
+	/**
+	 * Directly sets the % power on the elevator motor.
+	 * If the elevator is uncalibrated, power will be constrained.
+	 * @param power
+	 */
+	public void setPower(double power) {
+	    
+	    boolean sensorHit = calibrationSensor.get();
+	    calibrationLatch.setValue(sensorHit);
+	    
+	    // If the lower-bound sensor is hit, then we need to prevent the mechanism from lowering any further.
+	    if (sensorHit) {
+	        power = MathUtils.constrainDouble(power, 0, 1);
+	    }
+	    
+	    // If the elevator is not calibrated, then maximum power should be constrained.
+	    if (!_isCalibrated) {
+	        power = MathUtils.constrainDouble(power, -calibrationPower.get(), calibrationPower.get());
+	    }
+	    
+	    motor.simpleSet(power);
 	}
 
 	/**
 	 * Raises the elevator. Power is controlled by a property.
 	 */
 	public void rise(){
-		motor.simpleSet(elevatorPower.get());
+	    setPower(elevatorPower.get());
 	}
+	
 	
 	/**
 	 * Lower the elevator. Power is controlled by a property.
 	 */
 	public void lower(){
-		motor.simpleSet(-elevatorPower.get());
+	    setPower(-elevatorPower.get());
 	}
 	
 	public void stop(){
-		motor.simpleSet(0);
+	    setPower(0);
 	}
 	
-	public void setPower(double power) {
-		motor.simpleSet(power);
+	public double currentHeight() {
+		return ticksToInches(motor.getSelectedSensorPosition(0));
 	}
 	
+	private double ticksToInches(double ticks) { 
+	    double tpi = elevatorTicksPerInch.get();
+	    if (tpi == 0) {
+	        return 0;
+	    }
+	    
+	    return ((ticks - calibrationOffset) / tpi) + minHeightInInches.get();
+	}
+	
+	public boolean isCalibrated() {
+	    return _isCalibrated;
+	}
+	
+	/**
+	 * Returns true if the elevator is close to its maximum height.
+	 */
+	
+	boolean isCloseToMaxmumHeight() {
+		
+		if (currentHeight() >= maxHeightInInches.get() * 0.9) {
+			return true;
+		}
+	
+		return false;
+		
+	}
+	
+	/**
+	 * Returns true if the elevator is close to its minimum height.
+	 */
+	
+	boolean isCloseToMinimumHeight() {
+		
+		if (currentHeight() < maxHeightInInches.get() * 0.15) {
+			return true;
+		}
+		
+		return false;
+	}	
 }
 	
---
+++ Competition2018/src/main/java/competition/subsystems/elevator/commands/RiseCommand.java
@@ -0,0 +1,31 @@
+package competition.subsystems.elevator.commands;
+
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+
+import competition.subsystems.elevator.ElevatorSubsystem;
+import xbot.common.command.BaseCommand;
+import xbot.common.command.BaseSubsystem;
+
+@Singleton
+public class RiseCommand extends BaseCommand {
+	
+	ElevatorSubsystem rise;
+	
+	@Inject
+	public RiseCommand(ElevatorSubsystem rise) {
+		this.rise = rise;
+	}
+	
+	@Override
+	public void initialize() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public void execute() {
+		rise.rise();
+	}
+	
+}
