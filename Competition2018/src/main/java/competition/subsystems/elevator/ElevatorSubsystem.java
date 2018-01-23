9e23b3d92aa462d4615b6081e569b40fa8905e02
John Gilbert <johngilb@gmail.com>
2018-01-22 17:33:01 -0800

    Finish up simple calibration and add some tests

--- Competition2018/src/main/java/competition/subsystems/elevator/ElevatorSubsystem.java
+++ Competition2018/src/main/java/competition/subsystems/elevator/ElevatorSubsystem.java
@@ -1,5 +1,6 @@
 package competition.subsystems.elevator;
 
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 
@@ -29,11 +30,10 @@ public class ElevatorSubsystem extends BaseSubsystem {
 	
 	private boolean _isCalibrated;
 	private double calibrationOffset;
-	private Latch calibrationLatch;
+	private final Latch calibrationLatch;
 	
-	double height;
-	double maxHeight;
-	double minHeight;
+	final DoubleProperty maxHeightInInches;
+	final DoubleProperty minHeightInInches;
 	
 	public XCANTalon motor;
 	public XDigitalInput calibrationSensor;
@@ -44,25 +44,47 @@ public class ElevatorSubsystem extends BaseSubsystem {
 		elevatorPower = propMan.createPersistentProperty("ElevatorPower", 0.4);
 		elevatorTicksPerInch = propMan.createPersistentProperty("ElevatorTicksPerInch", 100);
 		calibrationPower = propMan.createPersistentProperty("ElevatorCalibrationPower", 0.2);
+		maxHeightInInches = propMan.createPersistentProperty("Elevator Max HeightInInches", 80);
+		minHeightInInches = propMan.createPersistentProperty("Elevator Min HeightInInches", 3);		
+		
 		calibrationOffset = 0;
+		
+		calibrationLatch = new Latch(false, EdgeType.RisingEdge, edge -> {
+		    if (edge == EdgeType.RisingEdge) {
+		        calibrate();
+		    }
+		});
 	}
 	
 	public void temporaryHack() {
 		motor = clf.createCANTalon(40);
+		motor.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder, 0, 0);
 		calibrationSensor = clf.createDigitalInput(1);
-		
-		calibrationLatch = new Latch(false, EdgeType.RisingEdge);
-		//calibrationLatch.addEdgeCallback(callback);
 	}
 	
+	private void calibrate() {
+	    calibrationOffset = motor.getSelectedSensorPosition(0);
+	    _isCalibrated = true;
+	}
+	
+	/**
+	 * Directly sets the % power on the elevator motor.
+	 * If the elevator is uncalibrated, power will be constrained.
+	 * @param power
+	 */
 	public void setPower(double power) {
 	    
+	    boolean sensorHit = calibrationSensor.get();
+	    calibrationLatch.setValue(sensorHit);
 	    
+	    // If the lower-bound sensor is hit, then we need to prevent the mechanism from lowering any further.
+	    if (sensorHit) {
+	        power = MathUtils.constrainDouble(power, 0, 1);
+	    }
 	    
+	    // If the elevator is not calibrated, then maximum power should be constrained.
 	    if (!_isCalibrated) {
-	        MathUtils.constrainDouble(power, -calibrationPower.get(), calibrationPower.get());
-	        
-	        _isCalibrated = calibrationSensor.get();
+	        power = MathUtils.constrainDouble(power, -calibrationPower.get(), calibrationPower.get());
 	    }
 	    
 	    motor.simpleSet(power);
@@ -97,7 +119,7 @@ public class ElevatorSubsystem extends BaseSubsystem {
 	        return 0;
 	    }
 	    
-	    return (ticks - calibrationOffset) / tpi;
+	    return ((ticks - calibrationOffset) / tpi) + minHeightInInches.get();
 	}
 	
 	public boolean isCalibrated() {
@@ -110,7 +132,7 @@ public class ElevatorSubsystem extends BaseSubsystem {
 	
 	boolean isCloseToMaxmumHeight() {
 		
-		if (height >= maxHeight * 0.9) {
+		if (currentHeight() >= maxHeightInInches.get() * 0.9) {
 			return true;
 		}
 	
@@ -124,7 +146,7 @@ public class ElevatorSubsystem extends BaseSubsystem {
 	
 	boolean isCloseToMinimumHeight() {
 		
-		if (height < maxHeight * 0.15) {
+		if (currentHeight() < maxHeightInInches.get() * 0.15) {
 			return true;
 		}
 		
--- Competition2018/src/test/java/competition/subsystems/elevator/ElevatorSubsystemTest.java
+++ Competition2018/src/test/java/competition/subsystems/elevator/ElevatorSubsystemTest.java
@@ -7,6 +7,8 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 import competition.BaseCompetitionTest;
+import edu.wpi.first.wpilibj.MockDigitalInput;
+import xbot.common.controls.actuators.mock_adapters.MockCANTalon;
 import xbot.common.injection.BaseWPITest;
 
 public class ElevatorSubsystemTest extends BaseCompetitionTest {
@@ -41,7 +43,43 @@ public class ElevatorSubsystemTest extends BaseCompetitionTest {
 		assertTrue(elevator.motor.getMotorOutputPercent() < 0);
 	}
 	
-	private void checkElevatorPower(double power) {
-		assertEquals(elevator.motor.getMotorOutputPercent(), power, 0.001);
+	@Test
+	public void testCantLowerIfLimitSwitchPressed() {
+	    ((MockDigitalInput)elevator.calibrationSensor).set_value(true);
+	    elevator.setPower(-1);
+	    
+	    checkElevatorPower(0);
 	}
+	
+	@Test
+	public void testTypicalCalibration() {
+	    // make sure we start uncalibrated at some random height
+	    ((MockCANTalon)elevator.motor).setPosition(1000);
+        assertTrue(elevator.currentHeight() > 5);
+	    assertFalse(elevator.isCalibrated());
+	    
+	    elevator.setPower(-1);
+	    // verify going down, but not at full power
+	    assertTrue(elevator.motor.getMotorOutputPercent() > -0.8);
+	    assertTrue(elevator.motor.getMotorOutputPercent() < -0.1);
+	    
+	    ((MockDigitalInput)elevator.calibrationSensor).set_value(true);
+	    elevator.setPower(-1);
+	    // verify that the system is now calibrated to the minimum height.
+	    assertTrue(elevator.isCalibrated());
+	    assertEquals(3, elevator.currentHeight(), 0.001);
+	    
+	    
+	    // have the system raise but the switch still depress - this should not continuously calibrate.
+	    // With the current hardcoded values, every 100 ticks is one inch of travel.
+	    ((MockCANTalon)elevator.motor).setPosition(2000);
+	    elevator.setPower(1);
+	    assertTrue(elevator.isCalibrated());
+        assertEquals(13, elevator.currentHeight(), 0.001);
+	}
+	
+	private void checkElevatorPower(double power) {
+		assertEquals(power, elevator.motor.getMotorOutputPercent(),  0.001);
+	}
+	
 }
