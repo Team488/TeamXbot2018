cb9206aac572b6b7c2f7e8cdbfd89433575e4f21
John Gilbert <johngilb@gmail.com>
2018-01-23 20:20:22 -0800

    clean up whitespace

--- Competition2018/src/main/java/competition/operator_interface/OperatorCommandMap.java
+++ Competition2018/src/main/java/competition/operator_interface/OperatorCommandMap.java
@@ -13,29 +13,19 @@ public class OperatorCommandMap {
 
     // Example for setting up a command to fire when a button is pressed:
     /*
+     * @Inject public void setupMyCommands( OperatorInterface operatorInterface, MyCommand myCommand) {
+     * operatorInterface.leftButtons.getifAvailable(1).whenPressed(myCommand); }
+     */
+
     @Inject
-    public void setupMyCommands(
-            OperatorInterface operatorInterface,
-            MyCommand myCommand)
-    {
-        operatorInterface.leftButtons.getifAvailable(1).whenPressed(myCommand);
+    public void setupShiftGearCommand(OperatorInterface oi, ToggleGearCommand shiftGear) {
+        oi.gamepad.getifAvailable(4).whenPressed(shiftGear);
     }
-    */
-	
-	@Inject
-	public void setupShiftGearCommand(
-			OperatorInterface oi,
-			ToggleGearCommand shiftGear
-			) {
-		oi.gamepad.getifAvailable(4).whenPressed(shiftGear);
-	}
-	
-	@Inject
-	public void setupDriveCommands(
-	        OperatorInterface oi,
-	        AssistedTankDriveCommand assistedTank,
-	        TankDriveWithJoysticksCommand simpleTank) {
-	    oi.gamepad.getifAvailable(9).whenPressed(assistedTank);
-	    oi.gamepad.getifAvailable(10).whenPressed(simpleTank);
-	}
+
+    @Inject
+    public void setupDriveCommands(OperatorInterface oi, AssistedTankDriveCommand assistedTank,
+            TankDriveWithJoysticksCommand simpleTank) {
+        oi.gamepad.getifAvailable(9).whenPressed(assistedTank);
+        oi.gamepad.getifAvailable(10).whenPressed(simpleTank);
+    }
 }
