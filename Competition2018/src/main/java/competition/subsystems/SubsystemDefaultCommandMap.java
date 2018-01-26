277abbdcb48de1db4472116a7e4b4af10157ee10
Mr-Recalculate <peregrineautumn@gmail.com>
2018-01-25 18:46:16 -0800

    wrote defaults (#27)

--- Competition2018/src/main/java/competition/subsystems/SubsystemDefaultCommandMap.java
+++ Competition2018/src/main/java/competition/subsystems/SubsystemDefaultCommandMap.java
@@ -5,6 +5,14 @@ import com.google.inject.Singleton;
 
 import competition.subsystems.drive.DriveSubsystem;
 import competition.subsystems.drive.commands.TankDriveWithJoysticksCommand;
+import competition.subsystems.climberdeploy.ClimberDeploySubsystem;
+import competition.subsystems.climberdeploy.commands.StopClimberArmCommand;
+import competition.subsystems.gripperdeploy.GripperDeploySubsystem;
+import competition.subsystems.gripperdeploy.commands.GripperStopDeployCommand;
+import competition.subsystems.lean.LeanSubsystem;
+import competition.subsystems.lean.commands.StopLeaningCommand;
+import competition.subsystems.shift.ShiftSubsystem;
+import competition.subsystems.shift.commands.ShiftLowCommand;
 
 @Singleton
 public class SubsystemDefaultCommandMap {
@@ -13,5 +21,25 @@ public class SubsystemDefaultCommandMap {
     @Inject
     public void setupDriveSubsystem(DriveSubsystem driveSubsystem, TankDriveWithJoysticksCommand command) {
         driveSubsystem.setDefaultCommand(command);
+    }
+    
+    @Inject
+    public void setupClimberDeploySubsystem(ClimberDeploySubsystem climberdeploySubsystem, StopClimberArmCommand command) {
+    	climberdeploySubsystem.setDefaultCommand(command);
+    }
+    
+    @Inject 
+    public void setupGripperDeploySubsystem(GripperDeploySubsystem gripperdeploySubsystem, GripperStopDeployCommand command) {
+    	gripperdeploySubsystem.setDefaultCommand(command);
+    }
+    
+    @Inject
+    public void setupLeanSubsystem(LeanSubsystem leanSubsystem, StopLeaningCommand command) {
+    	leanSubsystem.setDefaultCommand(command);
+    }
+    
+    @Inject
+    public void setupShiftSubsytem(ShiftSubsystem shiftSubsystem, ShiftLowCommand command) {
+    	shiftSubsystem.setDefaultCommand(command);
     }
 }
