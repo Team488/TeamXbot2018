405dab0bedf9209073bbae1603c2fec0828e2de1
John Gilbert <johngilb@gmail.com>
2018-02-08 15:49:46 -0800

    Clean up PR from feedback Remove unnecessary maintainerCommand clean up tabs

--- Competition2018/src/main/java/competition/subsystems/elevator/commands/MoveElevatorToGivenHeightForTimeCommand.java
+++ Competition2018/src/main/java/competition/subsystems/elevator/commands/MoveElevatorToGivenHeightForTimeCommand.java
@@ -9,51 +9,51 @@ import xbot.common.properties.DoubleProperty;
 import xbot.common.properties.XPropertyManager;
 
 public class MoveElevatorToGivenHeightForTimeCommand extends BaseCommand {
-	private ElevatorSubsystem elevator;
-	
-	public DoubleProperty errorThreshold;
-	public DoubleProperty stableTimeRequired;
-	
-	double initialTime;
-	public int target;
-	
-	boolean previouslyAtTarget;
+    private ElevatorSubsystem elevator;
 
-	@Inject
-	public MoveElevatorToGivenHeightForTimeCommand(ElevatorSubsystem subsystem, XPropertyManager propMan) {
-		this.elevator = subsystem;
-		errorThreshold = propMan.createPersistentProperty("Elevator delta height threshold", 0.1);
-		stableTimeRequired = propMan.createPersistentProperty("Required time on target for elevator in seconds", 0.5);
-		previouslyAtTarget = false;
-		this.requires(subsystem);
-	}
+    public DoubleProperty errorThreshold;
+    public DoubleProperty stableTimeRequired;
 
-	public void setTargetHeight(int height) {
-		this.target = height;
-	}
+    double initialTime;
+    public int target;
 
-	@Override
-	public void initialize() {
-		elevator.setTargetHeight(target);
-	}
+    boolean previouslyAtTarget;
 
-	@Override
-	public void execute() {
-		double error = Math.abs(elevator.currentHeight()- elevator.getTargetHeight());
-		if (error <= errorThreshold.get()) {
-			if (!previouslyAtTarget) {
-				initialTime = Timer.getFPGATimestamp();
-				previouslyAtTarget = true;
-			}
-		} else {
-			previouslyAtTarget = false;
-		} 
-	}
+    @Inject
+    public MoveElevatorToGivenHeightForTimeCommand(ElevatorSubsystem subsystem, XPropertyManager propMan) {
+        this.elevator = subsystem;
+        errorThreshold = propMan.createPersistentProperty("Elevator delta height threshold", 0.1);
+        stableTimeRequired = propMan.createPersistentProperty("Required time on target for elevator in seconds", 0.5);
+        previouslyAtTarget = false;
+        this.requires(subsystem);
+    }
 
-	@Override
-	public boolean isFinished() {
-		double currentTime = Timer.getFPGATimestamp();
-		return previouslyAtTarget && (currentTime - initialTime) >= stableTimeRequired.get();
-	}
+    public void setTargetHeight(int height) {
+        this.target = height;
+    }
+
+    @Override
+    public void initialize() {
+        elevator.setTargetHeight(target);
+    }
+
+    @Override
+    public void execute() {
+        double error = Math.abs(elevator.getCurrentHeightInInches() - elevator.getTargetHeight());
+        if (error <= errorThreshold.get()) {
+            if (!previouslyAtTarget) {
+                initialTime = Timer.getFPGATimestamp();
+                previouslyAtTarget = true;
+            }
+        } else {
+            previouslyAtTarget = false;
+        }
+    }
+
+    @Override
+    public boolean isFinished() {
+        double currentTime = Timer.getFPGATimestamp();
+        return previouslyAtTarget && (currentTime - initialTime) >= stableTimeRequired.get();
+    }
 
 }
--- Competition2018/src/test/java/competition/subsystems/elevator/commands/MoveElevatorToGivenHeightForTimeCommandTest.java
+++ Competition2018/src/test/java/competition/subsystems/elevator/commands/MoveElevatorToGivenHeightForTimeCommandTest.java
@@ -11,43 +11,43 @@ import edu.wpi.first.wpilibj.MockTimer;
 import xbot.common.controls.actuators.mock_adapters.MockCANTalon;
 
 public class MoveElevatorToGivenHeightForTimeCommandTest extends BaseCompetitionTest {
-	ElevatorSubsystem elevator;
-	MoveElevatorToGivenHeightForTimeCommand command;
-	MockTimer mockTimer;
+    ElevatorSubsystem elevator;
+    MoveElevatorToGivenHeightForTimeCommand command;
+    MockTimer mockTimer;
 
-	@Override
-	public void setUp() {
-		super.setUp();
+    @Override
+    public void setUp() {
+        super.setUp();
 
-		command = injector.getInstance(MoveElevatorToGivenHeightForTimeCommand.class);
-		elevator = injector.getInstance(ElevatorSubsystem.class);
-		mockTimer = injector.getInstance(MockTimer.class);
-		elevator.temporaryHack();
-	}
+        command = injector.getInstance(MoveElevatorToGivenHeightForTimeCommand.class);
+        elevator = injector.getInstance(ElevatorSubsystem.class);
+        mockTimer = injector.getInstance(MockTimer.class);
+        elevator.temporaryHack();
+    }
 
-	@Test
-	public void testCommand() {
-		command.setTargetHeight(80);
-		command.initialize();
-		mockTimer.setTimeInSeconds(0);
-		
-		// at target height and required time met --> should be true
-		((MockCANTalon) elevator.motor).setPosition(7700);
-		command.execute();
-		mockTimer.advanceTimeInSecondsBy(command.stableTimeRequired.get());
-		assertTrue(command.isFinished());
+    @Test
+    public void testCommand() {
+        command.setTargetHeight(80);
+        command.initialize();
+        mockTimer.setTimeInSeconds(0);
 
-		// required time for meeting target met but not at target --> should be false
-		((MockCANTalon) elevator.motor).setPosition(20);
-		command.execute();
-		mockTimer.advanceTimeInSecondsBy(command.stableTimeRequired.get());
-		assertFalse(command.isFinished());
+        // at target height and required time met --> should be true
+        ((MockCANTalon) elevator.motor).setPosition(7700);
+        command.execute();
+        mockTimer.advanceTimeInSecondsBy(command.stableTimeRequired.get());
+        assertTrue(command.isFinished());
 
-		// required time for meeting target not met but at target --> should be false
-		mockTimer.advanceTimeInSecondsBy(command.stableTimeRequired.get() - 0.3);
-		((MockCANTalon) elevator.motor).setPosition(7700);
-		command.execute();
-		assertFalse(command.isFinished());
-	}
+        // required time for meeting target met but not at target --> should be false
+        ((MockCANTalon) elevator.motor).setPosition(20);
+        command.execute();
+        mockTimer.advanceTimeInSecondsBy(command.stableTimeRequired.get());
+        assertFalse(command.isFinished());
+
+        // required time for meeting target not met but at target --> should be false
+        mockTimer.advanceTimeInSecondsBy(command.stableTimeRequired.get() - 0.3);
+        ((MockCANTalon) elevator.motor).setPosition(7700);
+        command.execute();
+        assertFalse(command.isFinished());
+    }
 
 }
