0932802e59193902596dba4966238f85cd34205e
veronayue <veronayue@gmail.com>
2018-02-08 20:49:01 -0800

    new method for calculating the average current with hashmap; getAverageCurrent and getPeakCurrent

--- Competition2018/src/main/java/competition/subsystems/elevator/commands/TrackCurrentUsageCommand.java
+++ Competition2018/src/main/java/competition/subsystems/elevator/commands/TrackCurrentUsageCommand.java
@@ -1,79 +1,59 @@
 package competition.subsystems.elevator.commands;
 
-import competition.subsystems.elevator.ElevatorSubsystem;
+import java.util.HashMap;
+
 import edu.wpi.first.wpilibj.Timer;
 import xbot.common.command.BaseCommand;
 import xbot.common.controls.actuators.XCANTalon;
-import xbot.common.properties.DoubleProperty;
 import xbot.common.properties.XPropertyManager;
 
-public class TrackCurrentUsageCommand extends BaseCommand{
+public class TrackCurrentUsageCommand extends BaseCommand {
 
-    ElevatorSubsystem elevator;
     XCANTalon talon;
-    double oldCurrent;
     double current;
-    double size;
-    double total;
-    double averageCurrent;
-    double targetTime;
-    double currentTime;
-    double initialTime;
-    double maxCurrentInAMinute;
-    double minCurrentInAMinute;   
+    double time;
+    double currentAveragingWindow;
 
+    HashMap<Double, Double> currentHistory;
 
-
-    public TrackCurrentUsageCommand(XPropertyManager propMan,XCANTalon talon, ElevatorSubsystem elevator) {
-        this.elevator = elevator;
+    public TrackCurrentUsageCommand(XPropertyManager propMan, XCANTalon talon) {
         this.talon = talon;
+        currentHistory = new HashMap<Double, Double>();
+        currentAveragingWindow = 1000;
     }
-
 
     @Override
     public void initialize() {
-
-        current=talon.getOutputCurrent();
-        size = 1;
-        total = current;
-        initialTime=Timer.getFPGATimestamp();
-        targetTime = initialTime+1;
-        maxCurrentInAMinute=currentTime;
-        minCurrentInAMinute=currentTime;
-
+        
     }
-    /**
-     * can calculate the average current within a second and the max and min current within a minute;
-     */
 
     @Override
     public void execute() {
-
-        currentTime=Timer.getFPGATimestamp();
-
-        while (currentTime<targetTime) {
-
-            current = talon.getOutputCurrent(); 
-            total = total + current;
-            size = size + 1;
-
-            //if it is beyond 60 seconds, the max and min number would go to zero; 
-            if (currentTime-initialTime >= 60 ) {
-                maxCurrentInAMinute=0;
-                minCurrentInAMinute=0;
-
+        current = talon.getOutputCurrent();
+        time = Timer.getFPGATimestamp();
+        currentHistory.put(time, current);
+        for (Double presentTime : currentHistory.keySet()) {
+            if (presentTime < time - currentAveragingWindow) {
+                currentHistory.remove(presentTime);
             }
-            if (current > maxCurrentInAMinute) {
-                maxCurrentInAMinute = current;
-            }
-
-            if (current< minCurrentInAMinute) {
-                minCurrentInAMinute = current;
-            }
-
-            averageCurrent = total / size;
         }
+    }
 
-        targetTime=currentTime+1;
+    public double getAverageCurrent() {
+        double sum = 0;
+        for (Double presentTime : currentHistory.keySet()) {
+            sum += currentHistory.get(presentTime);
+        }
+        return sum / currentHistory.size();
+    }
+    
+    public double getPeakCurrent() {
+        double peakCurrent = talon.getOutputCurrent();
+        for(Double presentTime : currentHistory.keySet()) {
+            if(peakCurrent < currentHistory.get(presentTime)) {
+                peakCurrent = currentHistory.get(presentTime);
+            }
+        }
+        return peakCurrent;
     }
 }
