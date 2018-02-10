c385d2869e3710109e1307ddad69356934b821f5
johnli <john.li9631@gmail.com>
2018-02-10 11:31:50 -0800

    Reformatted DriveForDistanceCommand

--- Competition2018/src/main/java/competition/subsystems/drive/commands/DriveForDistanceCommand.java
+++ Competition2018/src/main/java/competition/subsystems/drive/commands/DriveForDistanceCommand.java
@@ -16,92 +16,92 @@ import competition.subsystems.pose.PoseSubsystem;
 
 public class DriveForDistanceCommand extends BaseCommand {
 
-	private final PIDManager travelManager;
-	private final PIDManager headingDrivePid;
-	private final PoseSubsystem poseSubsystem;
-	private final DriveSubsystem drive;
-	private final HeadingModule heading;
+    private final PIDManager travelManager;
+    private final PIDManager headingDrivePid;
+    private final PoseSubsystem poseSubsystem;
+    private final DriveSubsystem drive;
+    private final HeadingModule heading;
 
-	private double deltaDistance;
-	private double targetDistance;
-	private double startingPosition;
-	public final double defaultPValue = 1 / 80d;
+    private double deltaDistance;
+    private double targetDistance;
+    private double startingPosition;
+    public final double defaultPValue = 1 / 80d;
 
-	private DoubleProperty deltaDistanceProp;
-	private ContiguousHeading targetHeading;
+    private DoubleProperty deltaDistanceProp;
+    private ContiguousHeading targetHeading;
 
-	@Inject
-	public DriveForDistanceCommand(DriveSubsystem driveSubsystem, XPropertyManager propManager,
-			RobotAssertionManager assertionManager, PIDFactory pidFactory, PoseSubsystem pose, CommonLibFactory clf) {
+    @Inject
+    public DriveForDistanceCommand(DriveSubsystem driveSubsystem, XPropertyManager propManager,
+            RobotAssertionManager assertionManager, PIDFactory pidFactory, PoseSubsystem pose, CommonLibFactory clf) {
 
-		this.drive = driveSubsystem;
-		this.poseSubsystem = pose;
-		this.requires(driveSubsystem);
-		this.travelManager = pidFactory.createPIDManager("Drive to position", 0.1, 0, 0, 0, 0.5, -0.5, 3, 1, 0.5);
-		headingDrivePid = pidFactory.createPIDManager("Heading module", defaultPValue, 0, 0);
-		targetHeading = new ContiguousHeading();
-		this.heading = clf.createHeadingModule(headingDrivePid);
+        this.drive = driveSubsystem;
+        this.poseSubsystem = pose;
+        this.requires(driveSubsystem);
+        this.travelManager = pidFactory.createPIDManager("Drive to position", 0.1, 0, 0, 0, 0.5, -0.5, 3, 1, 0.5);
+        headingDrivePid = pidFactory.createPIDManager("Heading module", defaultPValue, 0, 0);
+        targetHeading = new ContiguousHeading();
+        this.heading = clf.createHeadingModule(headingDrivePid);
 
-	}
+    }
 
-	/**
-	 * Sets the target distance
-	 * 
-	 * @param deltaDistance
-	 *            the target in inches
-	 */
-	public void setDeltaDistance(double deltaDistance) {
-		this.deltaDistance = deltaDistance;
-	}
+    /**
+     * Sets the target distance
+     * 
+     * @param deltaDistance
+     *            the target in inches
+     */
+    public void setDeltaDistance(double deltaDistance) {
+        this.deltaDistance = deltaDistance;
+    }
 
-	/**
-	 * Sets the target distance with a DoubleProperty
-	 * 
-	 * @param deltaDistanceProp
-	 *            the DoubleProperty for the target
-	 */
-	public void setDeltaDistance(DoubleProperty deltaDistanceProp) {
-		this.deltaDistanceProp = deltaDistanceProp;
-	}
+    /**
+     * Sets the target distance with a DoubleProperty
+     * 
+     * @param deltaDistanceProp
+     *            the DoubleProperty for the target
+     */
+    public void setDeltaDistance(DoubleProperty deltaDistanceProp) {
+        this.deltaDistanceProp = deltaDistanceProp;
+    }
 
-	@Override
-	public void initialize() {
-		startingPosition = getYDistance();
+    @Override
+    public void initialize() {
+        startingPosition = getYDistance();
 
-		targetHeading = poseSubsystem.getCurrentHeading();
+        targetHeading = poseSubsystem.getCurrentHeading();
 
-		if (deltaDistanceProp != null) {
-			this.targetDistance = getYDistance() + deltaDistanceProp.get();
-		} else {
-			this.targetDistance = getYDistance() + deltaDistance;
-		}
-		log.info("Initializing  with distance " + targetDistance + " inches");
-	}
+        if (deltaDistanceProp != null) {
+            this.targetDistance = getYDistance() + deltaDistanceProp.get();
+        } else {
+            this.targetDistance = getYDistance() + deltaDistance;
+        }
+        log.info("Initializing  with distance " + targetDistance + " inches");
+    }
 
-	@Override
-	public void execute() {
-		double power = travelManager.calculate(targetDistance, getYDistance());
-		double headingPower = heading.calculateHeadingPower(targetHeading.getValue());
+    @Override
+    public void execute() {
+        double power = travelManager.calculate(targetDistance, getYDistance());
+        double headingPower = heading.calculateHeadingPower(targetHeading.getValue());
 
-		double leftPower = power - headingPower;
-		double rightPower = power + headingPower;
+        double leftPower = power - headingPower;
+        double rightPower = power + headingPower;
 
-		drive.drive(leftPower, rightPower);
-	}
+        drive.drive(leftPower, rightPower);
+    }
 
-	private double getYDistance() {
-		return poseSubsystem.getRobotOrientedTotalDistanceTraveled().y;
-	}
+    private double getYDistance() {
+        return poseSubsystem.getRobotOrientedTotalDistanceTraveled().y;
+    }
 
-	@Override
-	public boolean isFinished() {
-		return travelManager.isOnTarget();
-	}
+    @Override
+    public boolean isFinished() {
+        return travelManager.isOnTarget();
+    }
 
-	@Override
-	public void end() {
-		log.info("Ending, PreviousPosition was " + startingPosition + ", Targeted Delta in distance is " + deltaDistance
-				+ " Distance travelled is " + (getYDistance() - startingPosition));
-		drive.stop();
-	}
+    @Override
+    public void end() {
+        log.info("Ending, PreviousPosition was " + startingPosition + ", Targeted Delta in distance is " + deltaDistance
+                + " Distance travelled is " + (getYDistance() - startingPosition));
+        drive.stop();
+    }
 }
