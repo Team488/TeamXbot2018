9843ad8af0b8954f7084cd44ada28502b2e64f62
johnli <john.li9631@gmail.com>
2018-02-10 11:28:51 -0800

    Reformatted with the correct format

--- Competition2018/src/main/java/competition/subsystems/elevator/ElevatorSubsystem.java
+++ Competition2018/src/main/java/competition/subsystems/elevator/ElevatorSubsystem.java
@@ -20,234 +20,233 @@ import xbot.common.properties.XPropertyManager;
 @Singleton
 public class ElevatorSubsystem extends BaseSetpointSubsystem implements PeriodicDataSource {
 
-	double defaultElevatorPower;
-	final CommonLibFactory clf;
-	final ElectricalContract2018 contract;
-	final DoubleProperty elevatorPower;
-	final DoubleProperty elevatorTicksPerInch;
+    double defaultElevatorPower;
+    final CommonLibFactory clf;
+    final ElectricalContract2018 contract;
+    final DoubleProperty elevatorPower;
+    final DoubleProperty elevatorTicksPerInch;
 
-	/**
-	 * If our elevator is uncalibrated, we don't allow large power inputs.
-	 */
-	final DoubleProperty calibrationPower;
+    /**
+     * If our elevator is uncalibrated, we don't allow large power inputs.
+     */
+    final DoubleProperty calibrationPower;
 
-	private boolean isCalibrated;
-	private double calibrationOffset;
-	private final Latch calibrationLatch;
+    private boolean isCalibrated;
+    private double calibrationOffset;
+    private final Latch calibrationLatch;
 
-	final DoubleProperty maxHeightInInches;
-	final DoubleProperty minHeightInInches;
-	final DoubleProperty elevatorTargetHeight;
-	final DoubleProperty currentTicks;
-	final DoubleProperty currentHeight;
-	final BooleanProperty lowerLimitSensor;
-	final BooleanProperty upperLimitSensor;
+    final DoubleProperty maxHeightInInches;
+    final DoubleProperty minHeightInInches;
+    final DoubleProperty elevatorTargetHeight;
+    final DoubleProperty currentTicks;
+    final DoubleProperty currentHeight;
+    final BooleanProperty lowerLimitSensor;
+    final BooleanProperty upperLimitSensor;
 
-	public XCANTalon motor;
-	public XDigitalInput lowerLimitSwitch;
-	public XDigitalInput upperLimitSwitch;
+    public XCANTalon motor;
+    public XDigitalInput lowerLimitSwitch;
+    public XDigitalInput upperLimitSwitch;
 
-	@Inject
-	public ElevatorSubsystem(CommonLibFactory clf, XPropertyManager propMan, ElectricalContract2018 contract) {
-		this.clf = clf;
-		this.contract = contract;
-		elevatorPower = propMan.createPersistentProperty("ElevatorPower", 0.4);
-		elevatorTicksPerInch = propMan.createPersistentProperty("ElevatorTicksPerInch", 100);
-		calibrationPower = propMan.createPersistentProperty("ElevatorCalibrationPower", 0.2);
-		maxHeightInInches = propMan.createPersistentProperty("Elevator Max HeightInInches", 80);
-		minHeightInInches = propMan.createPersistentProperty("Elevator Min HeightInInches", 3);
-		elevatorTargetHeight = propMan.createEphemeralProperty("targetHeight", maxHeightInInches.get());
-		currentTicks = propMan.createEphemeralProperty("Elevator current ticks", 0.0);
-		currentHeight = propMan.createEphemeralProperty("Elevator current height", 0.0);
-		lowerLimitSensor = propMan.createEphemeralProperty("Elevator Lower Limit", false);
-		upperLimitSensor = propMan.createEphemeralProperty("Elevator Upper Limit", false);
+    @Inject
+    public ElevatorSubsystem(CommonLibFactory clf, XPropertyManager propMan, ElectricalContract2018 contract) {
+        this.clf = clf;
+        this.contract = contract;
+        elevatorPower = propMan.createPersistentProperty("ElevatorPower", 0.4);
+        elevatorTicksPerInch = propMan.createPersistentProperty("ElevatorTicksPerInch", 100);
+        calibrationPower = propMan.createPersistentProperty("ElevatorCalibrationPower", 0.2);
+        maxHeightInInches = propMan.createPersistentProperty("Elevator Max HeightInInches", 80);
+        minHeightInInches = propMan.createPersistentProperty("Elevator Min HeightInInches", 3);
+        elevatorTargetHeight = propMan.createEphemeralProperty("targetHeight", maxHeightInInches.get());
+        currentTicks = propMan.createEphemeralProperty("Elevator current ticks", 0.0);
+        currentHeight = propMan.createEphemeralProperty("Elevator current height", 0.0);
+        lowerLimitSensor = propMan.createEphemeralProperty("Elevator Lower Limit", false);
+        upperLimitSensor = propMan.createEphemeralProperty("Elevator Upper Limit", false);
 
-		calibrationOffset = 0.0;
+        calibrationOffset = 0.0;
 
-		calibrationLatch = new Latch(false, EdgeType.RisingEdge, edge -> {
-			if (edge == EdgeType.RisingEdge) {
-				calibrateHere();
-			}
-		});
+        calibrationLatch = new Latch(false, EdgeType.RisingEdge, edge -> {
+            if (edge == EdgeType.RisingEdge) {
+                calibrateHere();
+            }
+        });
 
-		if (contract.elevatorReady()) {
-			initializeMotor();
-		}
+        if (contract.elevatorReady()) {
+            initializeMotor();
+        }
 
-		if (contract.elevatorLowerLimitReady()) {
-			initializeLowerLimit();
-		}
+        if (contract.elevatorLowerLimitReady()) {
+            initializeLowerLimit();
+        }
 
-		if (contract.elevatorUpperLimitReady()) {
-			initializeUpperLimit();
-		}
+        if (contract.elevatorUpperLimitReady()) {
+            initializeUpperLimit();
+        }
 
-	}
+    }
 
-	private void initializeMotor() {
-		motor = clf.createCANTalon(contract.getElevatorMaster().channel);
-		motor.setInverted(contract.getElevatorMaster().inverted);
-		motor.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, 0);
-		motor.setSensorPhase(true);
+    private void initializeMotor() {
+        motor = clf.createCANTalon(contract.getElevatorMaster().channel);
+        motor.setInverted(contract.getElevatorMaster().inverted);
+        motor.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, 0);
+        motor.setSensorPhase(true);
 
-		motor.createTelemetryProperties("ElevatorMotor");
-	}
+        motor.createTelemetryProperties("ElevatorMotor");
+    }
 
-	private void initializeLowerLimit() {
-		lowerLimitSwitch = clf.createDigitalInput(contract.getElevatorLowerLimit().channel);
-		lowerLimitSwitch.setInverted(contract.getElevatorLowerLimit().inverted);
-	}
+    private void initializeLowerLimit() {
+        lowerLimitSwitch = clf.createDigitalInput(contract.getElevatorLowerLimit().channel);
+        lowerLimitSwitch.setInverted(contract.getElevatorLowerLimit().inverted);
+    }
 
-	private void initializeUpperLimit() {
-		upperLimitSwitch = clf.createDigitalInput(contract.getElevatorUpperLimit().channel);
-		upperLimitSwitch.setInverted(contract.getElevatorUpperLimit().inverted);
-	}
+    private void initializeUpperLimit() {
+        upperLimitSwitch = clf.createDigitalInput(contract.getElevatorUpperLimit().channel);
+        upperLimitSwitch.setInverted(contract.getElevatorUpperLimit().inverted);
+    }
 
-	public void calibrateHere() {
-		calibrateAt(motor.getSelectedSensorPosition(0));
-	}
+    public void calibrateHere() {
+        calibrateAt(motor.getSelectedSensorPosition(0));
+    }
 
-	public void calibrateAt(double lowestPosition) {
-		log.info("Calibrating elevator with lowest position of " + lowestPosition);
-		calibrationOffset = lowestPosition;
-		isCalibrated = true;
-	}
+    public void calibrateAt(double lowestPosition) {
+        log.info("Calibrating elevator with lowest position of " + lowestPosition);
+        calibrationOffset = lowestPosition;
+        isCalibrated = true;
+    }
 
-	public void uncalibrate() {
-		isCalibrated = false;
-	}
+    public void uncalibrate() {
+        isCalibrated = false;
+    }
 
-	public boolean isCalibrated() {
-		return isCalibrated;
-	}
+    public boolean isCalibrated() {
+        return isCalibrated;
+    }
 
-	/**
-	 * Directly sets the % power on the elevator motor. If the elevator is
-	 * uncalibrated, power will be constrained.
-	 * 
-	 * @param power
-	 *            power percentage in robot scale
-	 */
-	public void setPower(double power) {
+    /**
+     * Directly sets the % power on the elevator motor. If the elevator is uncalibrated, power will be constrained.
+     * 
+     * @param power
+     *            power percentage in robot scale
+     */
+    public void setPower(double power) {
 
-		if (contract.elevatorLowerLimitReady()) {
-			boolean sensorHit = lowerLimitSwitch.get();
-			calibrationLatch.setValue(sensorHit);
+        if (contract.elevatorLowerLimitReady()) {
+            boolean sensorHit = lowerLimitSwitch.get();
+            calibrationLatch.setValue(sensorHit);
 
-			// If the lower-bound sensor is hit, then we need to prevent the mechanism from
-			// lowering any further.
-			if (sensorHit) {
-				power = MathUtils.constrainDouble(power, 0, 1);
-			}
-		}
+            // If the lower-bound sensor is hit, then we need to prevent the mechanism from
+            // lowering any further.
+            if (sensorHit) {
+                power = MathUtils.constrainDouble(power, 0, 1);
+            }
+        }
 
-		if (contract.elevatorUpperLimitReady()) {
-			boolean sensorHit = upperLimitSwitch.get();
+        if (contract.elevatorUpperLimitReady()) {
+            boolean sensorHit = upperLimitSwitch.get();
 
-			// If the upper-bound sensor is hit, then we need to prevent the mechanism from
-			// rising any further.
-			if (sensorHit) {
-				power = MathUtils.constrainDouble(power, -1, 0);
-			}
-		}
+            // If the upper-bound sensor is hit, then we need to prevent the mechanism from
+            // rising any further.
+            if (sensorHit) {
+                power = MathUtils.constrainDouble(power, -1, 0);
+            }
+        }
 
-		// If the elevator is not calibrated, then maximum power should be constrained.
-		if (!isCalibrated) {
-			power = MathUtils.constrainDouble(power, -calibrationPower.get(), calibrationPower.get());
-		}
+        // If the elevator is not calibrated, then maximum power should be constrained.
+        if (!isCalibrated) {
+            power = MathUtils.constrainDouble(power, -calibrationPower.get(), calibrationPower.get());
+        }
 
-		if (isCalibrated) {
-			// if we are above the max, only go down.
-			double currentHeight = getCurrentHeightInInches();
-			if (currentHeight > getMaxHeightInInches()) {
-				power = MathUtils.constrainDouble(power, -1, 0);
-			}
-			// if we are below the min, can only go up.
-			if (currentHeight < getMinHeightInInches()) {
-				power = MathUtils.constrainDouble(power, 0, 1);
-			}
-		}
+        if (isCalibrated) {
+            // if we are above the max, only go down.
+            double currentHeight = getCurrentHeightInInches();
+            if (currentHeight > getMaxHeightInInches()) {
+                power = MathUtils.constrainDouble(power, -1, 0);
+            }
+            // if we are below the min, can only go up.
+            if (currentHeight < getMinHeightInInches()) {
+                power = MathUtils.constrainDouble(power, 0, 1);
+            }
+        }
 
-		motor.simpleSet(power);
-	}
+        motor.simpleSet(power);
+    }
 
-	public void setTargetHeight(double height) {
-		elevatorTargetHeight.set(height);
-	}
+    public void setTargetHeight(double height) {
+        elevatorTargetHeight.set(height);
+    }
 
-	public double getTargetHeight() {
-		return elevatorTargetHeight.get();
-	}
+    public double getTargetHeight() {
+        return elevatorTargetHeight.get();
+    }
 
-	/**
-	 * Raises the elevator. Power is controlled by a property.
-	 */
-	public void rise() {
-		setPower(elevatorPower.get());
-	}
+    /**
+     * Raises the elevator. Power is controlled by a property.
+     */
+    public void rise() {
+        setPower(elevatorPower.get());
+    }
 
-	/**
-	 * Lower the elevator. Power is controlled by a property.
-	 */
-	public void lower() {
-		setPower(-elevatorPower.get());
-	}
+    /**
+     * Lower the elevator. Power is controlled by a property.
+     */
+    public void lower() {
+        setPower(-elevatorPower.get());
+    }
 
-	public void stop() {
-		setPower(0);
-	}
+    public void stop() {
+        setPower(0);
+    }
 
-	public double getCurrentHeightInInches() {
-		return ticksToInches(motor.getSelectedSensorPosition(0));
-	}
+    public double getCurrentHeightInInches() {
+        return ticksToInches(motor.getSelectedSensorPosition(0));
+    }
 
-	public double getCurrentTick() {
-		return motor.getSelectedSensorPosition(0);
-	}
+    public double getCurrentTick() {
+        return motor.getSelectedSensorPosition(0);
+    }
 
-	public double getMinHeightInInches() {
-		return minHeightInInches.get();
-	}
+    public double getMinHeightInInches() {
+        return minHeightInInches.get();
+    }
 
-	public double getMaxHeightInInches() {
-		return maxHeightInInches.get();
-	}
+    public double getMaxHeightInInches() {
+        return maxHeightInInches.get();
+    }
 
-	public void setTickPerInch(double ticksPerInch) {
-		elevatorTicksPerInch.set(ticksPerInch);
-	}
+    public void setTickPerInch(double ticksPerInch) {
+        elevatorTicksPerInch.set(ticksPerInch);
+    }
 
-	private double ticksToInches(double ticks) {
-		double tpi = elevatorTicksPerInch.get();
-		if (tpi == 0) {
-			return 0;
-		}
+    private double ticksToInches(double ticks) {
+        double tpi = elevatorTicksPerInch.get();
+        if (tpi == 0) {
+            return 0;
+        }
 
-		return ((ticks - calibrationOffset) / tpi) + minHeightInInches.get();
-	}
+        return ((ticks - calibrationOffset) / tpi) + minHeightInInches.get();
+    }
 
-	public double getMaxHeight() {
-		return maxHeightInInches.get();
-	}
+    public double getMaxHeight() {
+        return maxHeightInInches.get();
+    }
 
-	public double getMinHeight() {
-		return minHeightInInches.get();
-	}
+    public double getMinHeight() {
+        return minHeightInInches.get();
+    }
 
-	@Override
-	public void updatePeriodicData() {
-		if (contract.elevatorReady()) {
-			currentTicks.set(getCurrentTick());
-			currentHeight.set(getCurrentHeightInInches());
-			motor.updateTelemetryProperties();
-		}
+    @Override
+    public void updatePeriodicData() {
+        if (contract.elevatorReady()) {
+            currentTicks.set(getCurrentTick());
+            currentHeight.set(getCurrentHeightInInches());
+            motor.updateTelemetryProperties();
+        }
 
-		if (contract.elevatorLowerLimitReady()) {
-			lowerLimitSensor.set(lowerLimitSwitch.get());
-		}
+        if (contract.elevatorLowerLimitReady()) {
+            lowerLimitSensor.set(lowerLimitSwitch.get());
+        }
 
-		if (contract.elevatorUpperLimitReady()) {
-			upperLimitSensor.set(upperLimitSwitch.get());
-		}
-	}
+        if (contract.elevatorUpperLimitReady()) {
+            upperLimitSensor.set(upperLimitSwitch.get());
+        }
+    }
 }
--- Competition2018/src/test/java/competition/subsystems/elevator/ElevatorSubsystemTest.java
+++ Competition2018/src/test/java/competition/subsystems/elevator/ElevatorSubsystemTest.java
@@ -12,80 +12,80 @@ import xbot.common.controls.actuators.mock_adapters.MockCANTalon;
 
 public class ElevatorSubsystemTest extends BaseCompetitionTest {
 
-	ElevatorSubsystem elevator;
+    ElevatorSubsystem elevator;
 
-	@Override
-	public void setUp() {
-		super.setUp();
-		this.elevator = injector.getInstance(ElevatorSubsystem.class);
-	}
+    @Override
+    public void setUp() {
+        super.setUp();
+        this.elevator = injector.getInstance(ElevatorSubsystem.class);
+    }
 
-	@Test
-	public void isStopped() {
-		elevator.setPower(1);
-		elevator.stop();
-		checkElevatorPower(0);
-	}
+    @Test
+    public void isStopped() {
+        elevator.setPower(1);
+        elevator.stop();
+        checkElevatorPower(0);
+    }
 
-	@Test
-	public void rise() {
-		assertEquals(elevator.motor.getMotorOutputPercent(), 0, 0.001);
-		elevator.rise();
-		assertTrue(elevator.motor.getMotorOutputPercent() > 0);
-	}
+    @Test
+    public void rise() {
+        assertEquals(elevator.motor.getMotorOutputPercent(), 0, 0.001);
+        elevator.rise();
+        assertTrue(elevator.motor.getMotorOutputPercent() > 0);
+    }
 
-	@Test
-	public void lower() {
-		assertEquals(elevator.motor.getMotorOutputPercent(), 0, 0.001);
-		elevator.lower();
-		assertTrue(elevator.motor.getMotorOutputPercent() < 0);
-	}
+    @Test
+    public void lower() {
+        assertEquals(elevator.motor.getMotorOutputPercent(), 0, 0.001);
+        elevator.lower();
+        assertTrue(elevator.motor.getMotorOutputPercent() < 0);
+    }
 
-	@Test
-	public void testCantLowerIfLowerLimitSwitchPressed() {
-		((MockDigitalInput) elevator.lowerLimitSwitch).setValue(true);
-		elevator.setPower(-1);
+    @Test
+    public void testCantLowerIfLowerLimitSwitchPressed() {
+        ((MockDigitalInput) elevator.lowerLimitSwitch).setValue(true);
+        elevator.setPower(-1);
 
-		checkElevatorPower(0);
-	}
+        checkElevatorPower(0);
+    }
 
-	@Test
-	public void testCantRiseIfUpperLimitSwitchPressed() {
-		((MockDigitalInput) elevator.upperLimitSwitch).setValue(true);
-		elevator.setPower(1);
+    @Test
+    public void testCantRiseIfUpperLimitSwitchPressed() {
+        ((MockDigitalInput) elevator.upperLimitSwitch).setValue(true);
+        elevator.setPower(1);
 
-		checkElevatorPower(0);
-	}
+        checkElevatorPower(0);
+    }
 
-	@Test
-	public void testTypicalCalibration() {
-		// make sure we start uncalibrated at some random height
-		((MockCANTalon) elevator.motor).setPosition(1000);
-		assertTrue(elevator.getCurrentHeightInInches() > 5);
-		assertFalse(elevator.isCalibrated());
+    @Test
+    public void testTypicalCalibration() {
+        // make sure we start uncalibrated at some random height
+        ((MockCANTalon) elevator.motor).setPosition(1000);
+        assertTrue(elevator.getCurrentHeightInInches() > 5);
+        assertFalse(elevator.isCalibrated());
 
-		elevator.setPower(-1);
-		// verify going down, but not at full power
-		assertTrue(elevator.motor.getMotorOutputPercent() > -0.8);
-		assertTrue(elevator.motor.getMotorOutputPercent() < -0.1);
+        elevator.setPower(-1);
+        // verify going down, but not at full power
+        assertTrue(elevator.motor.getMotorOutputPercent() > -0.8);
+        assertTrue(elevator.motor.getMotorOutputPercent() < -0.1);
 
-		((MockDigitalInput) elevator.lowerLimitSwitch).setValue(true);
-		elevator.setPower(-1);
-		// verify that the system is now calibrated to the minimum height.
-		assertTrue(elevator.isCalibrated());
-		assertEquals(3, elevator.getCurrentHeightInInches(), 0.001);
+        ((MockDigitalInput) elevator.lowerLimitSwitch).setValue(true);
+        elevator.setPower(-1);
+        // verify that the system is now calibrated to the minimum height.
+        assertTrue(elevator.isCalibrated());
+        assertEquals(3, elevator.getCurrentHeightInInches(), 0.001);
 
-		// have the system raise but the switch still depress - this should not
-		// continuously calibrate.
-		// With the current hardcoded values, every 100 ticks is one inch of travel.
-		((MockCANTalon) elevator.motor).setPosition(2000);
-		elevator.setPower(1);
-		assertTrue(elevator.isCalibrated());
-		assertEquals(13, elevator.getCurrentHeightInInches(), 0.001);
-	}
+        // have the system raise but the switch still depress - this should not
+        // continuously calibrate.
+        // With the current hardcoded values, every 100 ticks is one inch of travel.
+        ((MockCANTalon) elevator.motor).setPosition(2000);
+        elevator.setPower(1);
+        assertTrue(elevator.isCalibrated());
+        assertEquals(13, elevator.getCurrentHeightInInches(), 0.001);
+    }
 
-	private void checkElevatorPower(double power) {
-		assertEquals(power, elevator.motor.getMotorOutputPercent(), 0.001);
-	}
+    private void checkElevatorPower(double power) {
+        assertEquals(power, elevator.motor.getMotorOutputPercent(), 0.001);
+    }
 
 }
