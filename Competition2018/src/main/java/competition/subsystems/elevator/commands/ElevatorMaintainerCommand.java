6534c327cb5044c83c3a6212344389b43d404d1f
Mr-Recalculate <peregrineautumn@gmail.com>
2018-02-13 18:32:38 -0800

    Auto-Calibrating Maintainer (#67)
    * added calibrate, maintainer, and manual modes along with tests for the calibrate command.
    
    * fixed some comments
    
    * fixed some comments
    
    * forget to press save

--- Competition2018/src/main/java/competition/subsystems/elevator/commands/ElevatorMaintainerCommand.java
+++ Competition2018/src/main/java/competition/subsystems/elevator/commands/ElevatorMaintainerCommand.java
@@ -3,18 +3,38 @@ package competition.subsystems.elevator.commands;
 import xbot.common.command.BaseCommand;
 import xbot.common.math.PIDFactory;
 import competition.subsystems.elevator.ElevatorSubsystem;
+import edu.wpi.first.wpilibj.Timer;
+
 import com.google.inject.Inject;
 import xbot.common.math.PIDManager;
+import xbot.common.properties.DoubleProperty;
+import xbot.common.properties.XPropertyManager;
+import competition.operator_interface.OperatorInterface;
 
 public class ElevatorMaintainerCommand extends BaseCommand {
+    
+    public enum MaintinerMode {
+        Calibrating,
+        GaveUp,
+        Calibrated
+    }
 
+    OperatorInterface oi;
     ElevatorSubsystem elevator;
     PIDManager pid;
+    double giveUpCalibratingTime;
+    final DoubleProperty elevatorCalibrationAttemptTimeMS;
 
     @Inject
-    public ElevatorMaintainerCommand(ElevatorSubsystem elevator, PIDFactory pf) {
+    public ElevatorMaintainerCommand(
+            ElevatorSubsystem elevator, 
+            PIDFactory pf, 
+            XPropertyManager propMan, 
+            OperatorInterface oi) {
+        elevatorCalibrationAttemptTimeMS = propMan.createPersistentProperty("Calibration attempt time (ms)", 4000);
         this.elevator = elevator;
         this.requires(elevator);
+        this.oi = oi;
         pid = pf.createPIDManager("Elevator", 1, 0, 0);
         pid.setErrorThreshold(0.1);
     }
@@ -24,16 +44,39 @@ public class ElevatorMaintainerCommand extends BaseCommand {
         log.info("Initializing");
         if (!elevator.isCalibrated()) {
             log.warn("ELEVATOR UNCALIBRATED - THIS COMMAND WILL NOT DO ANYTHING!");
+            giveUpCalibratingTime = Timer.getFPGATimestamp() + elevatorCalibrationAttemptTimeMS.get();
+            log.info("Attempting calibration from " + Timer.getFPGATimestamp() + " until " + giveUpCalibratingTime);
         }
     }
 
     @Override
     public void execute() {
+        boolean maintain = false;
+        boolean manual = false;
+        boolean tryToCalibrate = false;
+        
+        MaintinerMode currentMode = MaintinerMode.Calibrating;
+        
         if (elevator.isCalibrated()) {
-            double power = pid.calculate(elevator.getTargetHeight(), elevator.getCurrentHeightInInches());
-            elevator.setPower(power);
-        } else {
-            elevator.stop();
+            currentMode = MaintinerMode.Calibrated;
         }
-    }
+        else if (Timer.getFPGATimestamp() < giveUpCalibratingTime) {
+            currentMode = MaintinerMode.Calibrating;
+        }
+        else {
+            currentMode = MaintinerMode.GaveUp;
+        }
+        
+        
+        
+        if (currentMode == MaintinerMode.Calibrated) {
+            elevator.setPower(pid.calculate(elevator.getTargetHeight(), elevator.getCurrentHeightInInches()));
+        }
+        else if (currentMode == MaintinerMode.Calibrating) {
+            elevator.lower();
+        }
+        else if (currentMode == MaintinerMode.GaveUp) {
+            elevator.setPower(oi.operatorGamepad.getRightStickY());
+        }
+    }  
 }
