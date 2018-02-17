55887bcd8883d46241eeca7173b1cf6f31157575
christiancruzfhs <christian.c1@live.com>
2018-02-17 10:22:40 -0800

    deleted unnecessary methods and commands in the lean subsystem. edited lean subsystem test. set LeanWithJoysticksCommand as default command

--- Competition2018/src/main/java/competition/subsystems/lean/LeanSubsystem.java
+++ Competition2018/src/main/java/competition/subsystems/lean/LeanSubsystem.java
@@ -7,15 +7,11 @@ import competition.ElectricalContract2018;
 import xbot.common.command.BaseSubsystem;
 import xbot.common.controls.actuators.XCANTalon;
 import xbot.common.injection.wpi_factories.CommonLibFactory;
-import xbot.common.properties.DoubleProperty;
 import xbot.common.properties.XPropertyManager;
 
 @Singleton
 public class LeanSubsystem extends BaseSubsystem {
 
-    double currentLeanSpeed;
-    final DoubleProperty fastLeanSpeed;
-    final DoubleProperty slowLeanSpeed;
     final CommonLibFactory clf;
     final ElectricalContract2018 contract;
 
@@ -25,9 +21,6 @@ public class LeanSubsystem extends BaseSubsystem {
     public LeanSubsystem(CommonLibFactory clf, XPropertyManager propMan, ElectricalContract2018 contract) {
         this.clf = clf;
         this.contract = contract;
-        slowLeanSpeed = propMan.createPersistentProperty("slowLeanSpeed", .1);
-        fastLeanSpeed = propMan.createPersistentProperty("fastLeanSpeed", .4);
-        currentLeanSpeed = fastLeanSpeed.get();
 
         if (contract.climbLeanReady()) {
             initializeMotor();
@@ -39,60 +32,11 @@ public class LeanSubsystem extends BaseSubsystem {
         motor.setInverted(contract.getClimbLeanMaster().inverted);
     }
 
-    /**
-     * makes the climb arm lean left
-     */
-    public void leanLeft() {
-        motor.simpleSet(currentLeanSpeed);
-    }
-
-    /**
-     * makes the climb arm lean right
-     */
-    public void leanRight() {
-        motor.simpleSet(-currentLeanSpeed);
-    }
-
-    /**
-     * moves the arm to center position
-     */
-    public void center() {
-
-    }
-
-    /**
-     * stops the climb arm from leaning
-     */
-    public void stopLean() {
-        motor.simpleSet(0);
-    }
-
-    /**
-     * sensor to determine if the arm has hit the bar
-     */
-    public boolean hitBar() {
-        return false;
-    }
-
-    /**
-     * increases leaning arm speed
-     */
-    public void increaseSpeed() {
-        currentLeanSpeed = fastLeanSpeed.get();
-    }
-
-    /**
-     * decreases leaning arm speed
-     */
-    public void decreaseSpeed() {
-        currentLeanSpeed = slowLeanSpeed.get();
-    }
-
     public void setPower(double power) {
         motor.simpleSet(power);
     }
 
-    public double getLeanSpeed() {
+    public double getPower() {
         return motor.getMotorOutputPercent();
     }
 }
