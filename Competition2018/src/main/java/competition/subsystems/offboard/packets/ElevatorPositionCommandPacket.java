500efd098259ef7499baaf99a6f2c5f6173df45c
imathno <imathno@hotmail.com>
2018-02-16 19:58:13 -0800

    Add Elevator position command packet type

---
+++ Competition2018/src/main/java/competition/subsystems/offboard/packets/ElevatorPositionCommandPacket.java
@@ -0,0 +1,38 @@
+package competition.subsystems.offboard.packets;
+
+import org.apache.log4j.Logger;
+
+public class ElevatorPositionCommandPacket {
+	
+	private static Logger log = Logger.getLogger(ElevatorPositionCommandPacket.class);
+	
+	public final int commandId;
+	public final ElevatorGoal elevatorGoal;
+	
+	public enum ElevatorGoal {
+		Switch, ScaleLow, ScaleMid, ScaleHigh
+	}
+	
+	public ElevatorPositionCommandPacket(byte[] packetData) {
+		this.commandId = packetData[0] & 0xFF;
+		
+		switch (packetData[1]) {
+			case 1: this.elevatorGoal = ElevatorGoal.Switch;
+				break;
+			case 2: this.elevatorGoal = ElevatorGoal.ScaleLow;
+				break;
+			case 3: this.elevatorGoal = ElevatorGoal.ScaleMid;
+				break;
+			case 4: this.elevatorGoal = ElevatorGoal.ScaleHigh;
+				break;
+			default:
+				this.elevatorGoal = null;
+				log.error("Elevator goal: " + packetData[1] + " is invalid");
+				break;
+		}
+	}
+
+	public static ElevatorPositionCommandPacket parse(byte[] packetData) {
+		return new ElevatorPositionCommandPacket(packetData);
+	}
+}
