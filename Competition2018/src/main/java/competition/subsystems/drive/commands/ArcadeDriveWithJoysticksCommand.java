39b5e3ff6567b43fd66e892014c2bfa0749cb6ad
veronayue <veronayue@gmail.com>
2018-02-17 15:16:25 -0800

    made a test for ArcadeDrive

--- Competition2018/src/main/java/competition/subsystems/drive/commands/ArcadeDriveWithJoysticksCommand.java
+++ Competition2018/src/main/java/competition/subsystems/drive/commands/ArcadeDriveWithJoysticksCommand.java
@@ -5,18 +5,26 @@ import com.google.inject.Inject;
 import competition.operator_interface.OperatorInterface;
 import competition.subsystems.drive.DriveSubsystem;
 import xbot.common.command.BaseCommand;
+import xbot.common.injection.wpi_factories.CommonLibFactory;
 import xbot.common.math.XYPair;
+import xbot.common.subsystems.drive.control_logic.HeadingAssistModule;
+import xbot.common.subsystems.drive.control_logic.HeadingModule;
 
 public class ArcadeDriveWithJoysticksCommand extends BaseCommand {
 
     final DriveSubsystem driveSubsystem;
     final OperatorInterface oi;
+    final HeadingAssistModule ham;
 
     @Inject
-    public ArcadeDriveWithJoysticksCommand(OperatorInterface oi, DriveSubsystem driveSubsystem) {
+    public ArcadeDriveWithJoysticksCommand(OperatorInterface oi, DriveSubsystem driveSubsystem, CommonLibFactory clf) {
         this.oi = oi;
         this.driveSubsystem = driveSubsystem;
         this.requires(this.driveSubsystem);
+
+        HeadingModule holdHeading = clf.createHeadingModule(driveSubsystem.getRotateToHeadingPid());
+        HeadingModule decayHeading = clf.createHeadingModule(driveSubsystem.getRotateDecayPid());
+        ham = clf.createHeadingAssistModule(holdHeading, decayHeading);
     }
 
     @Override
@@ -26,7 +34,12 @@ public class ArcadeDriveWithJoysticksCommand extends BaseCommand {
 
     @Override
     public void execute() {
-        driveSubsystem.drive(new XYPair(0,oi.driverGamepad.getLeftVector().y), oi.driverGamepad.getRightVector().x);
+
+        double left = oi.driverGamepad.getLeftVector().y;
+        double right = oi.driverGamepad.getLeftVector().x;
+        double yTranslate = (left+right) / 2;
+        double turn = ham.calculateHeadingPower((right-left)/2);
+        driveSubsystem.drive(new XYPair(0, yTranslate), turn);
     }
 
 }
