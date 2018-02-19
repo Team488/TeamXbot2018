2814d0f744d0c497e386e77f6556cce37d94e33d
veronayue <veronayue@gmail.com>
2018-02-19 10:33:27 -0800

    change to elevator.lower & small changes

--- Competition2018/src/main/java/competition/subsystems/elevator/commands/CalibrateElevatorViaStallCommand.java
+++ Competition2018/src/main/java/competition/subsystems/elevator/commands/CalibrateElevatorViaStallCommand.java
@@ -6,63 +6,55 @@ import competition.subsystems.elevator.ElevatorSubsystem;
 import edu.wpi.first.wpilibj.Timer;
 import xbot.common.command.BaseCommand;
 import xbot.common.controls.sensors.TalonCurrentMonitor;
-import xbot.common.injection.wpi_factories.CommonLibFactory;
 import xbot.common.properties.DoubleProperty;
 import xbot.common.properties.XPropertyManager;
 
 public class CalibrateElevatorViaStallCommand extends BaseCommand {
     final ElevatorSubsystem elevator;
-    CommonLibFactory clf;
     TalonCurrentMonitor currentMonitor;
 
-    DoubleProperty power;
     DoubleProperty calibrationTime; 
     DoubleProperty calibrationCurrentThreshold;
     DoubleProperty goodCurrentRange;
 
     double targetTime;
-    double newTime;
     boolean atTarget;
     double currentThreshold;
     double peakCurrent;
 
 
     @Inject
-    public CalibrateElevatorViaStallCommand(XPropertyManager propMan, CommonLibFactory clf, ElevatorSubsystem elevator) {
-        this.clf = clf;
-        power = propMan.createPersistentProperty("Elevator Jamming Calibration Power", -0.2);
+    public CalibrateElevatorViaStallCommand(XPropertyManager propMan, ElevatorSubsystem elevator) {
         calibrationTime = propMan.createPersistentProperty("Elevator Calibration time (s)", 4);
         calibrationCurrentThreshold = propMan.createPersistentProperty("Calibration Current Threshold", 15);
         this.elevator = elevator;
         this.currentMonitor = new TalonCurrentMonitor(elevator.motor);
         this.requires(elevator);
     }
-    
+
     @Override
     public void initialize() {
         log.info("Initializing");
         log.info("Current time is: " + Timer.getFPGATimestamp());
-        log.info("Calibrating elevator at power" + power.get() + " until: " + targetTime);
         this.currentThreshold = calibrationCurrentThreshold.get();
         targetTime = Timer.getFPGATimestamp() + calibrationTime.get();
     }
 
     @Override
     public void execute() {
-            elevator.setPower(power.get());
-            currentMonitor.updateCurrent();
+        elevator.lower();
+        currentMonitor.updateCurrent();
     }
-    
+
     @Override
     public boolean isFinished() {
-        newTime =Timer.getFPGATimestamp();
-        return newTime > targetTime
+        return Timer.getFPGATimestamp() > targetTime
                 || currentMonitor.calculatePeakCurrent() > currentThreshold;
     }
-    
+
     @Override
     public void end() {
-         elevator.calibrateHere();
+        elevator.calibrateHere();
         elevator.stop();
     }
 
