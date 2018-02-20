e69d60e7f35b3df801f3baad3435659a8802e8be
John Gilbert <johngilb@gmail.com>
2018-02-19 18:52:46 -0800

    Make the wrist into a maintainer system - use dpad to control it.

---
+++ Competition2018/src/test/java/competition/subsystems/wrist/commands/WristMaintainerCommandTest.java
@@ -0,0 +1,55 @@
+package competition.subsystems.wrist.commands;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import competition.BaseCompetitionTest;
+import competition.subsystems.wrist.WristSubsystem;
+import xbot.common.controls.actuators.mock_adapters.MockCANTalon;
+
+public class WristMaintainerCommandTest extends BaseCompetitionTest {
+
+    WristMaintainerCommand command;
+    WristSubsystem wrist;
+    
+    @Override
+    public void setUp() {
+        super.setUp();
+        command = injector.getInstance(WristMaintainerCommand.class);
+        wrist = injector.getInstance(WristSubsystem.class);
+    }
+    
+    @Test
+    public void simpleTest() {
+        command.initialize();
+        command.execute();
+    }
+    
+    @Test
+    public void testMaintain() {
+        wrist.setTargetAngle(45);
+        wrist.calibrateHere();
+        
+        command.initialize();
+        command.execute();
+        
+        assertEquals(-wrist.getMaximumAllowedPower(), wrist.motor.getMotorOutputPercent(), 0.001);
+        
+        ((MockCANTalon)wrist.motor).setPosition(-45);
+        assertEquals(45, wrist.getWristAngle(), 0.001);
+        
+        command.execute();
+        assertEquals(0, wrist.motor.getMotorOutputPercent(), 0.001);
+    }
+    
+    @Test
+    public void uncalibrated() {
+        wrist.setTargetAngle(45);
+        
+        command.initialize();
+        command.execute();
+        
+        assertEquals(0, wrist.motor.getMotorOutputPercent(), 0.001);
+    }
+}
