db1afcf279164564aca9ff7bbcae4d94b0915715
imathno <imathno@hotmail.com>
2018-02-20 19:28:21 -0800

    Make some changes

--- Competition2018/src/main/java/competition/subsystems/offboard/packets/ElevatorPositionCommandPacket.java
+++ Competition2018/src/main/java/competition/subsystems/offboard/packets/ElevatorPositionCommandPacket.java
@@ -5,33 +5,39 @@ import org.apache.log4j.Logger;
 import competition.subsystems.elevator.ElevatorSubsystem.ElevatorGoal;
 
 public class ElevatorPositionCommandPacket {
-	
-	private static Logger log = Logger.getLogger(ElevatorPositionCommandPacket.class);
-	
-	public final int commandId;
-	public final ElevatorGoal elevatorGoal;
-	
-	public ElevatorPositionCommandPacket(byte[] packetData) {
-		this.commandId = packetData[0] & 0xFF;
-		
-		switch (packetData[1]) {
-			case 1: this.elevatorGoal = ElevatorGoal.PickUpHeight;
-				break;
-			case 2: this.elevatorGoal = ElevatorGoal.Switch;
-				break;
-			case 3: this.elevatorGoal = ElevatorGoal.ScaleLow;
-				break;
-			case 4: this.elevatorGoal = ElevatorGoal.ScaleMid;
-				break;
-			case 5: this.elevatorGoal = ElevatorGoal.ScaleHigh;
-				break;
-			default: this.elevatorGoal = null;
-				log.error("Elevator goal: " + packetData[1] + " is invalid");
-				break;
-		}
-	}
 
-	public static ElevatorPositionCommandPacket parse(byte[] packetData) {
-		return new ElevatorPositionCommandPacket(packetData);
-	}
+    private static Logger log = Logger.getLogger(ElevatorPositionCommandPacket.class);
+
+    public final int commandId;
+    public final ElevatorGoal elevatorGoal;
+
+    public ElevatorPositionCommandPacket(byte[] packetData) {
+        this.commandId = packetData[0] & 0xFF;
+
+        switch (packetData[1]) {
+        case 1:
+            this.elevatorGoal = ElevatorGoal.PickUpHeight;
+            break;
+        case 2:
+            this.elevatorGoal = ElevatorGoal.Switch;
+            break;
+        case 3:
+            this.elevatorGoal = ElevatorGoal.ScaleLow;
+            break;
+        case 4:
+            this.elevatorGoal = ElevatorGoal.ScaleMid;
+            break;
+        case 5:
+            this.elevatorGoal = ElevatorGoal.ScaleHigh;
+            break;
+        default:
+            this.elevatorGoal = null;
+            log.error("Elevator goal: " + packetData[1] + " is invalid");
+            break;
+        }
+    }
+
+    public static ElevatorPositionCommandPacket parse(byte[] packetData) {
+        return new ElevatorPositionCommandPacket(packetData);
+    }
 }
