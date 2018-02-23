5bea2c6034c66b1e3b0014007abd3851fa1fe223
JohnGilb <johngilb@gmail.com>
2018-02-22 21:34:28 -0800

    Feature/switch scoring auton (#105)
    * Upgrade the DynamicScoreOnSwitchCommandGroup to actually score points in addition to driving.
    
    * Multi-level switch to decide what to do - this will have to be revisited

--- Competition2018/src/main/java/competition/commandgroups/DynamicScoreOnSwitchCommandGroup.java
+++ Competition2018/src/main/java/competition/commandgroups/DynamicScoreOnSwitchCommandGroup.java
@@ -3,6 +3,11 @@ package competition.commandgroups;
 import com.google.inject.Inject;
 
 import competition.subsystems.autonomous.AutonomousDecisionSystem;
+import competition.subsystems.elevator.ElevatorSubsystem;
+import competition.subsystems.elevator.commands.SetElevatorTargetHeightCommand;
+import competition.subsystems.gripperintake.commands.GripperEjectCommand;
+import competition.subsystems.wrist.commands.SetWristAngleCommand;
+import openrio.powerup.MatchData.GameFeature;
 import xbot.common.command.BaseCommandGroup;
 import xbot.common.subsystems.drive.PurePursuitCommand;
 
@@ -11,11 +16,25 @@ public class DynamicScoreOnSwitchCommandGroup extends BaseCommandGroup {
     public PurePursuitCommand pursuit;
     
     @Inject
-    public DynamicScoreOnSwitchCommandGroup(AutonomousDecisionSystem decider,
-            PurePursuitCommand pursuit) {
+    public DynamicScoreOnSwitchCommandGroup(
+            AutonomousDecisionSystem decider,
+            ElevatorSubsystem elevator,
+            PurePursuitCommand pursuit,
+            SetWristAngleCommand setWristDown,
+            SetElevatorTargetHeightCommand setElevatorForSwitch,
+            GripperEjectCommand eject) {
         this.pursuit = pursuit;
-        pursuit.setPointSupplier(decider.getAutoPath());
+        pursuit.setPointSupplier(decider.getAutoPathToFeature(GameFeature.SWITCH_NEAR));
         
+        setWristDown.setGoalAngle(0);
+        setElevatorForSwitch.setGoalHeight(elevator.getTargetSwitchDropHeight());
+        
+        // Get ready to score
+        this.addParallel(setWristDown);
+        this.addParallel(setElevatorForSwitch);
         this.addSequential(pursuit);
+        
+        // Score for 1 second
+        this.addSequential(eject, 1);
     }
 }
