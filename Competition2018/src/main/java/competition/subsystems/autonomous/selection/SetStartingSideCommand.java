20bc07358ce7977a50b8e57fdf0edeb1a18784e8
John Gilbert <johngilb@gmail.com>
2018-02-25 19:54:57 -0800

    Add some commands to set the DynamicScore commands as the autonomous command.
    Command command command. =]

---
+++ Competition2018/src/main/java/competition/subsystems/autonomous/selection/SetStartingSideCommand.java
@@ -0,0 +1,36 @@
+package competition.subsystems.autonomous.selection;
+
+import com.google.inject.Inject;
+
+import competition.subsystems.autonomous.AutonomousDecisionSystem;
+import xbot.common.command.BaseCommand;
+
+public class SetStartingSideCommand extends BaseCommand {
+
+    AutonomousDecisionSystem decider;
+    boolean isRightSide = true;
+    
+    @Inject
+    public SetStartingSideCommand(AutonomousDecisionSystem decider) {
+        this.decider = decider;
+    }
+    
+    public void setRightSide(boolean robotOnRightSide) {
+        isRightSide = robotOnRightSide;
+    }
+    
+    @Override
+    public void initialize() {
+        log.info("Initializing with isRightSide:" + isRightSide);
+        decider.setRobotPosition(isRightSide);
+    }
+
+    @Override
+    public void execute() {
+    }
+
+    @Override
+    public boolean isFinished() {
+        return true;
+    }
+}
