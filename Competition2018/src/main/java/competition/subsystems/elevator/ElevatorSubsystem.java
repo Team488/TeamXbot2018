39cc3267f774b29d79412d4f261ec373df421fb6
veronayue <veronayue@gmail.com>
2018-02-27 19:45:04 -0800

    set defult

--- Competition2018/src/main/java/competition/subsystems/elevator/ElevatorSubsystem.java
+++ Competition2018/src/main/java/competition/subsystems/elevator/ElevatorSubsystem.java
@@ -98,7 +98,7 @@ public class ElevatorSubsystem extends BaseSetpointSubsystem implements Periodic
         this.contract = contract;
         elevatorPower = propMan.createPersistentProperty(getPrefix() + "Standard Power", 0.4);
         elevatorTicksPerInch = propMan.createPersistentProperty(getPrefix() + "TicksPerInch", 100);
-        calibrationPower = propMan.createPersistentProperty(getPrefix() + "CalibrationPower", 0.2);
+        calibrationPower = propMan.createPersistentProperty(getPrefix() + "CalibrationPower", 0.3);
         maxHeightInInches = propMan.createPersistentProperty(getPrefix() + "Max HeightInInches", 80);
         minHeightInInches = propMan.createPersistentProperty(getPrefix() + "Min HeightInInches", 3);
         elevatorTargetHeight = propMan.createEphemeralProperty(getPrefix() + "TargetHeight", maxHeightInInches.get());
@@ -109,10 +109,10 @@ public class ElevatorSubsystem extends BaseSetpointSubsystem implements Periodic
         targetScaleHighHeight = propMan.createPersistentProperty(getPrefix() + "Scale high", 76.5);
         targetScaleMidHeight = propMan.createPersistentProperty(getPrefix() + "Scale mid", 64.5);
         targetScaleLowHeight = propMan.createPersistentProperty(getPrefix() + "Scale low", 60.0);
-        targetSwitchDropHeight = propMan.createPersistentProperty(getPrefix() + "Switch drop height", 19.0);
+        targetSwitchDropHeight = propMan.createPersistentProperty(getPrefix() + "Switch drop height", 35);
         targetPickUpHeight = propMan.createPersistentProperty(getPrefix() + "Pickup height", 3.0);
-        elevatorPeakCurrentLimit = propMan.createPersistentProperty(getPrefix() + "Peak current limit", 35);
-        elevatorPeakCurrentDuration = propMan.createPersistentProperty(getPrefix() + "Peak current duration", 200);
+        elevatorPeakCurrentLimit = propMan.createPersistentProperty(getPrefix() + "Peak current limit", 40);
+        elevatorPeakCurrentDuration = propMan.createPersistentProperty(getPrefix() + "Peak current duration", 3000);
         elevatorContinuousCurrentLimit = propMan.createPersistentProperty(getPrefix() + "Continuous current limit", 30);
         motionMagicProperties = pf.createPIDPropertyManager(getPrefix() + "Motion Magic", 0.3, 0, 0, 0.688);
         talonMaxVelocity = propMan.createPersistentProperty(getPrefix() + "Max Velocity", 1400);
@@ -124,8 +124,8 @@ public class ElevatorSubsystem extends BaseSetpointSubsystem implements Periodic
                 minHeightInInches.get() + 10);
         heightNearHighLimit = propMan.createPersistentProperty(getPrefix() + "Height Near High Limit",
                 maxHeightInInches.get() - 10);
-        powerNearLowLimit = propMan.createPersistentProperty(getPrefix() + "Max Power Near Low Limit", 0.3);
-        powerNearHighLimit = propMan.createPersistentProperty(getPrefix() + "Max Power Near High Limit", 0.3);
+        powerNearLowLimit = propMan.createPersistentProperty(getPrefix() + "Max Power Near Low Limit", 0.2);
+        powerNearHighLimit = propMan.createPersistentProperty(getPrefix() + "Max Power Near High Limit", 0.5);
         positionalPid = pf.createPIDManager(getPrefix() + "Position", 0.1, 0, 0);
         velocityPid = pf.createPIDManager(getPrefix() + "Velocity", 0.004, 0, 0);
         currentVelocity = propMan.createEphemeralProperty(getPrefix() + "Current Velocity", 0);
