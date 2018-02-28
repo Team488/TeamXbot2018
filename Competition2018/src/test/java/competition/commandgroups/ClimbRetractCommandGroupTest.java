7614979c0acf0633cafa68346765cd63edee7f88
christiancruzfhs <christian.c1@live.com>
2018-02-27 19:52:59 -0800

    ClimbRetractCommandGroup

---
+++ Competition2018/src/test/java/competition/commandgroups/ClimbRetractCommandGroupTest.java
@@ -0,0 +1,42 @@
+package competition.commandgroups;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import competition.BaseCompetitionTest;
+import competition.subsystems.climb.ClimbSubsystem;
+import xbot.common.command.XScheduler;
+
+public class ClimbRetractCommandGroupTest extends BaseCompetitionTest{
+    
+    XScheduler xScheduler;
+    ClimbRetractCommandGroup command;
+    ClimbSubsystem climb;
+    
+    @Override
+    public void setUp() {
+        super.setUp();
+        this.climb = injector.getInstance(ClimbSubsystem.class);
+        this.command = injector.getInstance(ClimbRetractCommandGroup.class);
+        this.xScheduler = injector.getInstance(XScheduler.class);
+    }
+    
+    @Test
+    public void test() {
+        command.start();
+        
+        xScheduler.run();
+        
+        assertFalse(climb.solenoidA.getAdjusted());
+        
+        xScheduler.run();
+        
+        assertTrue(climb.solenoidB.getAdjusted());
+        
+        assertEquals(1.0, climb.motor.getMotorOutputPercent(), .001);
+    }
+    
+}
