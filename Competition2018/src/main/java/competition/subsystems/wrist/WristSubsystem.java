8d1afa8862a30aab0cf6bbba660a4799d458f985
Kaelin Laundry <wasabifan@outlook.com>
2018-03-01 17:19:12 -0800

    Add height-based safety zone on wrist to protect ZED (#129)
    * Add height-based safety zone on wrist to protect ZED
    
    * =
    
    * update periodically

--- Competition2018/src/main/java/competition/subsystems/wrist/WristSubsystem.java
+++ Competition2018/src/main/java/competition/subsystems/wrist/WristSubsystem.java
@@ -6,6 +6,7 @@ import com.google.inject.Inject;
 import com.google.inject.Singleton;
 
 import competition.ElectricalContract2018;
+import competition.subsystems.elevator.ElevatorSubsystem;
 import xbot.common.command.BaseSetpointSubsystem;
 import xbot.common.command.PeriodicDataSource;
 import xbot.common.controls.actuators.XCANTalon;
@@ -29,13 +30,20 @@ public class WristSubsystem extends BaseSetpointSubsystem implements PeriodicDat
     final DoubleProperty wristUncalibratedPowerProp;
     final DoubleProperty targetAngle;
 
+    private final BooleanProperty safetyZoneEnabled;
+    private final DoubleProperty safetyZoneMaxAngle;
+    private final DoubleProperty safetyZoneStartHeight;
+
     int lowerLimit;
     int upperLimit;
     boolean calibrated = false;
+    
+    private final ElevatorSubsystem elevator;
 
     @Inject
-    WristSubsystem(CommonLibFactory clf, XPropertyManager propMan, ElectricalContract2018 contract) {
+    WristSubsystem(CommonLibFactory clf, ElevatorSubsystem elevator, XPropertyManager propMan, ElectricalContract2018 contract) {
         this.clf = clf;
+        this.elevator = elevator;
         this.contract = contract;
         maximumWristPower = propMan.createPersistentProperty(getPrefix() + "Maximum Power", 0.3);
 
@@ -44,7 +52,11 @@ public class WristSubsystem extends BaseSetpointSubsystem implements PeriodicDat
         wristCalibratedProp = propMan.createEphemeralProperty(getPrefix() + "Calibrated", false);
         wristUncalibratedPowerProp = propMan.createPersistentProperty(getPrefix() + "Calibration power", 0.3);
         targetAngle = propMan.createEphemeralProperty(getPrefix() + "Target Angle", contract.getWristMaximumAngle());
-
+        
+        safetyZoneEnabled = propMan.createPersistentProperty(getPrefix() + "Safety zone/Is enabled", true);
+        safetyZoneMaxAngle = propMan.createPersistentProperty(getPrefix() + "Safety zone/Max angle", 50);
+        safetyZoneStartHeight = propMan.createPersistentProperty(getPrefix() + "Safety zone/Start height (in)", 40);
+        
         if (contract.wristReady()) {
             initializeMotor();
         }
@@ -55,6 +67,7 @@ public class WristSubsystem extends BaseSetpointSubsystem implements PeriodicDat
     }
 
     public void setTargetAngle(double angle) {
+        angle = modifyAngleForSafeties(angle);
         targetAngle.set(angle);
     }
 
@@ -150,8 +163,24 @@ public class WristSubsystem extends BaseSetpointSubsystem implements PeriodicDat
         } else {
             power = MathUtils.constrainDouble(power, -maximumWristPower.get(), maximumWristPower.get());
         }
-
+        
+        if (isWithinSafetyZone()) {
+            power = MathUtils.constrainDouble(power, -1, 0);
+        }
+        
         motor.simpleSet(power);
+    }
+    
+    public boolean isWithinSafetyZone() {
+        return safetyZoneEnabled.get() && elevator.getCurrentHeightInInches() >= this.safetyZoneStartHeight.get();
+    }
+    
+    public double modifyAngleForSafeties(double angle) {
+        if (isWithinSafetyZone()) {
+            return Math.min(angle, safetyZoneMaxAngle.get());
+        }
+        
+        return angle;
     }
 
     /**
@@ -187,5 +216,10 @@ public class WristSubsystem extends BaseSetpointSubsystem implements PeriodicDat
         motor.updateTelemetryProperties();
         wristCalibratedProp.set(calibrated);
         currentWristAngleProp.set(getWristAngle());
+        
+        if (isWithinSafetyZone()) {
+            double newTargetAngle = modifyAngleForSafeties(getTargetAngle());
+            targetAngle.set(newTargetAngle);
+        }
     }
 }
