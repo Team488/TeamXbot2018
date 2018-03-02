5d87a712437b7ffdb6aa9f56ea76e1d15e7aae15
johnli <john.li9631@gmail.com>
2018-03-01 19:58:31 -0800

    Created constructor for both OverrideCommand

--- Competition2018/src/main/java/competition/subsystems/elevator/commands/ElevatorDangerousOverrideCommand.java
+++ Competition2018/src/main/java/competition/subsystems/elevator/commands/ElevatorDangerousOverrideCommand.java
@@ -1,5 +1,7 @@
 package competition.subsystems.elevator.commands;
 
+import com.google.inject.Inject;
+
 import competition.operator_interface.OperatorInterface;
 import competition.subsystems.elevator.ElevatorSubsystem;
 import xbot.common.command.BaseCommand;
@@ -8,6 +10,13 @@ public class ElevatorDangerousOverrideCommand extends BaseCommand {
     
     ElevatorSubsystem elevator;
     OperatorInterface oi;
+    
+    @Inject
+    public ElevatorDangerousOverrideCommand(ElevatorSubsystem elevator, OperatorInterface oi) {
+        this.elevator = elevator;
+        this.oi = oi;
+        this.requires(elevator);
+    }
 
     @Override
     public void initialize() {
--- Competition2018/src/main/java/competition/subsystems/wrist/commands/WristDangerousOverrideCommand.java
+++ Competition2018/src/main/java/competition/subsystems/wrist/commands/WristDangerousOverrideCommand.java
@@ -1,5 +1,7 @@
 package competition.subsystems.wrist.commands;
 
+import com.google.inject.Inject;
+
 import competition.operator_interface.OperatorInterface;
 import competition.subsystems.wrist.WristSubsystem;
 import xbot.common.command.BaseCommand;
@@ -7,6 +9,13 @@ import xbot.common.command.BaseCommand;
 public class WristDangerousOverrideCommand extends BaseCommand {
     WristSubsystem wrist;
     OperatorInterface oi;
+    
+    @Inject
+    public WristDangerousOverrideCommand(WristSubsystem wrist, OperatorInterface oi) {
+        this.wrist = wrist;
+        this.oi = oi;
+        this.requires(wrist);
+    }
     
     @Override
     public void initialize() {
