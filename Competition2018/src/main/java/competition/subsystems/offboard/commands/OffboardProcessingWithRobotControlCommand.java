58a8293383529e79dfc65f15975988c39b26f9e9
imathno <imathno@hotmail.com>
2018-03-06 19:54:18 -0800

    Add command packets to parse data from the matrix and do stuff

--- Competition2018/src/main/java/competition/subsystems/offboard/commands/OffboardProcessingWithRobotControlCommand.java
+++ Competition2018/src/main/java/competition/subsystems/offboard/commands/OffboardProcessingWithRobotControlCommand.java
@@ -2,27 +2,39 @@ package competition.subsystems.offboard.commands;
 
 import competition.subsystems.drive.DriveSubsystem;
 import competition.subsystems.elevator.ElevatorSubsystem;
+import competition.subsystems.gripperintake.GripperIntakeSubsystem;
 import competition.subsystems.offboard.OffboardCommsConstants;
 import competition.subsystems.offboard.OffboardCommunicationPacket;
 import competition.subsystems.offboard.OffboardInterfaceSubsystem;
 import competition.subsystems.offboard.packets.DrivePowerCommandPacket;
 import competition.subsystems.offboard.packets.DriveVelCommandPacket;
 import competition.subsystems.offboard.packets.ElevatorPositionCommandPacket;
+import competition.subsystems.offboard.packets.GripperCommandPacket;
+import competition.subsystems.offboard.packets.WristCommandPacket;
+import competition.subsystems.wrist.WristSubsystem;
 
 public abstract class OffboardProcessingWithRobotControlCommand extends OffboardProcessingCommand {
     
     private final DriveSubsystem driveSubsystem;
     private final ElevatorSubsystem elevatorSubsystem;
+    private final WristSubsystem wristSubsystem;
+    private final GripperIntakeSubsystem gripperIntakeSubsystem;
 
     protected OffboardProcessingWithRobotControlCommand(int commandId, 
             OffboardInterfaceSubsystem offboardSubsystem, 
             DriveSubsystem driveSubsystem,
-            ElevatorSubsystem elevatorSubsystem) {
+            ElevatorSubsystem elevatorSubsystem,
+            WristSubsystem wristSubsystem,
+            GripperIntakeSubsystem gripperIntakeSubsystem) {
         super(commandId, offboardSubsystem);
         this.driveSubsystem = driveSubsystem;
         this.requires(driveSubsystem);
         this.elevatorSubsystem = elevatorSubsystem;
         this.requires(elevatorSubsystem);
+        this.wristSubsystem = wristSubsystem;
+        this.requires(wristSubsystem);
+        this.gripperIntakeSubsystem = gripperIntakeSubsystem;
+        this.requires(gripperIntakeSubsystem);
     }
     
     @Override
@@ -75,6 +87,36 @@ public abstract class OffboardProcessingWithRobotControlCommand extends Offboard
             }
             else {
                 log.warn("Received \"set elevator position command\" packet for command which is not currently running");
+            }
+        }
+        else if(packet.packetType == OffboardCommsConstants.PACKET_TYPE_WRIST_COMMAND) {
+            try {
+                WristCommandPacket wristCommandPacket = WristCommandPacket.parse(packet.data);
+                if (wristCommandPacket.commandId == this.commandId) {
+                    // TODO: Log occasionally
+                    this.wristSubsystem.setTargetAngle(wristCommandPacket.position);
+                }
+                else {
+                    log.warn("Received \"wrist command\" packet for command which is not currently running");
+                }
+            }
+            catch (IllegalArgumentException e) {
+                log.warn("Wrist command packet failed to parse");
+            }
+        }
+        else if(packet.packetType == OffboardCommsConstants.PACKET_TYPE_GRIPPER_CONTROL_COMMAND) {
+            try {
+                GripperCommandPacket gripperCommandPacket = GripperCommandPacket.parse(packet.data);
+                if (gripperCommandPacket.commandId == this.commandId) {
+                    // TODO: Log occasionally
+                    this.gripperIntakeSubsystem.setIntakeMode(gripperCommandPacket.gripperMode);
+                }
+                else {
+                    log.warn("Received \"gripper intake command\" packet for command which is not currently running");
+                }
+            }
+            catch (IllegalArgumentException e) {
+                log.warn("Gripper command packet failed to parse");
             }
         }
         else {
