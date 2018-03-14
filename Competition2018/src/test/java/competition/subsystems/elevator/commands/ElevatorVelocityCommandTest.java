d80207d5371647fbe9e092e2db0b4721dbe43c80
veronayue <veronayue@gmail.com>
2018-03-13 20:40:47 -0700

    working on test

--- Competition2018/src/test/java/competition/subsystems/elevator/commands/ElevatorVelocityCommandTest.java
+++ Competition2018/src/test/java/competition/subsystems/elevator/commands/ElevatorVelocityCommandTest.java
@@ -1,20 +1,24 @@
 package competition.subsystems.elevator.commands;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
+
+
 import competition.BaseCompetitionTest;
-import competition.subsystems.drive.DriveSubsystem.Side;
 import competition.subsystems.elevator.ElevatorSubsystem;
 import xbot.common.controls.actuators.mock_adapters.MockCANTalon;
+
 
 public class ElevatorVelocityCommandTest extends BaseCompetitionTest{
     
     ElevatorVelocityCommand command;
     ElevatorSubsystem elevator;
     MockCANTalon talon;
+    double oldThrottle;
+    double delta1;
+    double delta2;
     
     @Override
     public void setUp() {
@@ -33,9 +37,28 @@ public class ElevatorVelocityCommandTest extends BaseCompetitionTest{
     public void testVelocity() {
         command.initialize();
         ((MockCANTalon) elevator.motor).setPosition(0);
+        elevator.setTargetHeight(15);
         command.execute();
+        System.out.println(command.throttle);
+        System.out.println(elevator.getCurrentHeightInInches());
+        
+        oldThrottle = command.throttle;
+        command.execute();
+        System.out.println(command.throttle);
+        System.out.println(elevator.getCurrentHeightInInches());
+        assertTrue(oldThrottle - command.throttle < 0);
         assertTrue(-0.2 <= command.throttle && command.throttle <= 1);
-        //assertEquals(
+        delta1 = command.throttle - oldThrottle;
+        
+        oldThrottle = command.throttle;
+        command.execute();
+        System.out.println(command.throttle);
+        System.out.println(elevator.getCurrentHeightInInches());
+        assertTrue(oldThrottle - command.throttle < 0);
+        assertTrue(-0.2 <= command.throttle && command.throttle <= 1);
+        delta2 = command.throttle - oldThrottle;
+        //assertTrue(delta1 > delta2);
+        
     }
     
 }
