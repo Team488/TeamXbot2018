0f5d8e54df392a49f8f7ea2ad8ab24a31ca349f5
veronayue <veronayue@gmail.com>
2018-03-13 18:18:30 -0700

    more tests

--- Competition2018/src/test/java/competition/subsystems/elevator/commands/ElevatorVelocityCommandTest.java
+++ Competition2018/src/test/java/competition/subsystems/elevator/commands/ElevatorVelocityCommandTest.java
@@ -4,11 +4,13 @@ import org.junit.Test;
 
 import competition.BaseCompetitionTest;
 import competition.subsystems.elevator.ElevatorSubsystem;
+import xbot.common.controls.actuators.mock_adapters.MockCANTalon;
 
 public class ElevatorVelocityCommandTest extends BaseCompetitionTest{
     
     ElevatorVelocityCommand command;
     ElevatorSubsystem elevator;
+    MockCANTalon talon;
     
     @Override
     public void setUp() {
@@ -17,9 +19,14 @@ public class ElevatorVelocityCommandTest extends BaseCompetitionTest{
         elevator = injector.getInstance(ElevatorSubsystem.class);
     }
 
-
     @Test
     public void testSimple() {
+        command.initialize();
+        command.execute();
+    }
+    
+    @Test
+    public void testVelocity() {
         command.initialize();
         command.execute();
     }
