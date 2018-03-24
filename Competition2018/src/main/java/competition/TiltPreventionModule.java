6e6c482fdce1b59e8384c8cac979bef2d976db0e
veronayue <31166404+veronayue@users.noreply.github.com>
2018-03-24 16:06:42 -0700

    Feature/tilt detection prevention (#106)
    * working on preventTilt
    
    * write some simple code for limit the max speed of both direction for preventing tilt
    
    * add configuration
    
    * delete the extends part
    
    * make it return double power, but need to work on the test
    
    * working om the test
    
    * finish the module and the test
    
    * print
    
    * getgyro
    
    * finish
    
    * changed based on tilting forward/backward, the pitch is nagetive/positive

---
+++ Competition2018/src/main/java/competition/TiltPreventionModule.java
@@ -0,0 +1,35 @@
+package competition;
+
+import xbot.common.properties.DoubleProperty;
+import xbot.common.properties.XPropertyManager;
+import com.google.inject.Inject;
+import competition.subsystems.pose.PoseSubsystem;
+
+
+public class TiltPreventionModule{
+
+    PoseSubsystem pose;
+    double goodPoint;
+    double goodHeading;
+    DoubleProperty pitchThrehold;
+
+
+    @Inject
+    public TiltPreventionModule(PoseSubsystem pose, XPropertyManager propManager) {
+        this.pose = pose;
+        pitchThrehold = propManager.createPersistentProperty("Absolute Value of Pitch Threhold", 10);
+    }
+
+    public double preventTilt (double potentialPower) {
+
+        if (pose.getRobotPitch() > pitchThrehold.get() && potentialPower > 0) {
+            return 0; 
+        }
+
+        if(pose.getRobotPitch() < -pitchThrehold.get() && potentialPower < 0) {
+            return 0 ; 
+        }
+
+        return potentialPower;
+    }
+}
