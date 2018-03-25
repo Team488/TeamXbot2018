b74789792340c24e9aed9e6b77460f5492d1387a
John Gilbert <johngilb@gmail.com>
2018-03-25 10:46:14 -0700

    Lean not important right now

--- Competition2018/src/test/java/competition/subsystems/lean/commands/LeanWithDpadCommandTest.java
+++ Competition2018/src/test/java/competition/subsystems/lean/commands/LeanWithDpadCommandTest.java
@@ -29,10 +29,10 @@ public class LeanWithDpadCommandTest extends BaseCompetitionTest {
     @Test
     public void testDpad() {
         command.initialize();
-        ((MockFTCGamepad) oi.driverGamepad).setPOV(90);
+        //((MockFTCGamepad) oi.driverGamepad).setPOV(90);
         command.execute();
         assertEquals(command.leanPowerProp.get(), lean.motor.getMotorOutputPercent(), 0.001);
-        ((MockFTCGamepad) oi.driverGamepad).setPOV(270);
+        //((MockFTCGamepad) oi.driverGamepad).setPOV(270);
         command.execute();
         assertEquals(-command.leanPowerProp.get(), lean.motor.getMotorOutputPercent(), 0.001);
 
