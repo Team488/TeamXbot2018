276779fe5c86a48ad0a596580fc0779c48ae415c
John Gilbert <johngilb@gmail.com>
2018-04-03 16:47:53 -0700

    Adds logging so we can analyze the performance of the autonomous programs

--- Competition2018/src/main/java/competition/subsystems/drive/commands/AbsolutePurePursuit2018Command.java
+++ Competition2018/src/main/java/competition/subsystems/drive/commands/AbsolutePurePursuit2018Command.java
@@ -3,12 +3,15 @@ package competition.subsystems.drive.commands;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
 
 import com.google.inject.Inject;
 
 import competition.subsystems.drive.DriveSubsystem;
+import competition.subsystems.pose.PoseSubsystem;
 import competition.subsystems.shift.ShiftSubsystem;
 import competition.subsystems.shift.ShiftSubsystem.Gear;
+import edu.wpi.first.wpilibj.Timer;
 import xbot.common.injection.wpi_factories.CommonLibFactory;
 import xbot.common.logic.VelocityThrottleModule;
 import xbot.common.math.FieldPose;
@@ -28,12 +31,14 @@ public class AbsolutePurePursuit2018Command extends PurePursuitCommand {
     final VelocityThrottleModule translationalThrottleModule;
     final DoubleProperty maxInchesPerSecondProp;
     private final DriveSubsystem drive;
+    private final PoseSubsystem pose;
     
     @Inject
-    public AbsolutePurePursuit2018Command(ShiftSubsystem shifter, CommonLibFactory clf, BasePoseSubsystem pose, DriveSubsystem drive,
+    public AbsolutePurePursuit2018Command(ShiftSubsystem shifter, CommonLibFactory clf, PoseSubsystem pose, DriveSubsystem drive,
             XPropertyManager propMan) {
         super(clf, pose, drive, propMan);
         this.drive = drive;
+        this.pose = pose;
         this.shifter = shifter;
         
         translationalThrottleModule =
@@ -96,6 +101,31 @@ public class AbsolutePurePursuit2018Command extends PurePursuitCommand {
         if (shifter.getGear() != desiredGear) {
             shifter.setGear(desiredGear);
         }
+        
+        logTelemetry(
+                pose.getCurrentFieldPose(), 
+                chaseData.target, 
+                chaseData.rabbit, 
+                drive.getVelocityInInchesPerSecond(), 
+                coercedSpeedGoal);
     }
-
+    
+    private void logTelemetry(FieldPose robot, FieldPose target, FieldPose rabbit, double velocity, double velocityGoal) {
+        List<Double> data = new ArrayList<>();
+        data.add(Timer.getFPGATimestamp());
+        data.add(robot.getPoint().x);
+        data.add(robot.getPoint().y);
+        data.add(robot.getHeading().getValue());
+        data.add(target.getPoint().x);
+        data.add(target.getPoint().y);
+        data.add(target.getHeading().getValue());
+        data.add(rabbit.getPoint().x);
+        data.add(rabbit.getPoint().y);
+        data.add(velocityGoal);
+        data.add(velocity);
+        
+        String formattedData = data.stream().map(d -> String.format("%.2f", d)).collect(Collectors.joining(","));
+        log.info(formattedData);
+    }
+    
 }
