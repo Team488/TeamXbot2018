bd5169b6ec7c5758ea6f98f98431e388de88ff4b
John Gilbert <johngilb@gmail.com>
2018-04-03 17:48:33 -0700

    Add a light subsystem that can be used to show off our vision!

---
+++ Competition2018/src/main/java/competition/subsystems/lights/LightsSubsystem.java
@@ -0,0 +1,27 @@
+package competition.subsystems.lights;
+
+import com.google.inject.Singleton;
+
+import competition.ElectricalContract2018;
+import xbot.common.command.BaseSubsystem;
+import xbot.common.controls.actuators.XRelay;
+import xbot.common.injection.wpi_factories.CommonLibFactory;
+
+@Singleton
+public class LightsSubsystem extends BaseSubsystem {
+
+    final XRelay light;
+    
+    public LightsSubsystem(CommonLibFactory clf, ElectricalContract2018 contract) {
+        light = clf.createRelay(contract.getAutonomousLight().channel);
+        light.setInverted(contract.getAutonomousLight().inverted);
+    }
+    
+    public void setOn(boolean value) {
+        if (value) {
+            light.setForward();
+        } else {
+            light.stop();
+        }   
+    }
+}
