4bb40a805af195230f3c0e872e8cfafa7f4e4297
WasabiFan <wasabifan@outlook.com>
2018-04-10 19:03:40 -0700

    Sort members of contract

--- Competition2018/src/main/java/competition/Competition2018Robot.java
+++ Competition2018/src/main/java/competition/Competition2018Robot.java
@@ -3,35 +3,6 @@ package competition;
 public class Competition2018Robot extends ElectricalContract2018 {
     
     @Override
-    public DeviceInfo getElevatorMaster() {
-        return new DeviceInfo(25, true);
-    }
-    
-    @Override
-    public DeviceInfo getElevatorFollower() {
-        return new DeviceInfo(22, false);
-    }
-    
-    public DeviceInfo getElevatorEncoder() {
-        return new DeviceInfo(0, true);
-    }
-    
-    @Override
-    public DeviceInfo getShifterSolenoid() {
-        return new DeviceInfo(0, false);
-    }
-    
-    @Override
-    public DeviceInfo getPawlSolenoidA() {
-        return new DeviceInfo(1, true);
-    }
-    
-    @Override
-    public DeviceInfo getPawlSolenoidB() {
-        return new DeviceInfo(2, true);
-    }
-    
-    @Override
     public boolean climbDeployReady() {
         return false;
     }
@@ -47,37 +18,76 @@ public class Competition2018Robot extends ElectricalContract2018 {
     }
     
     @Override
-    public boolean isWristLimitsReady() {
+    public boolean collectorReady() {
+        return true;
+    }
+    
+    @Override
+    public boolean elevatorLowerLimitReady() {
+        return true;
+    }
+    
+    @Override
+    public boolean elevatorReady() {
+        return true;
+    }
+    
+    @Override
+    public boolean elevatorUpperLimitReady() {
+        return true;
+    }
+    
+    @Override
+    public boolean elevatorUsesTalonLimits() {
         return false;
     }
     
     @Override
-    public double getWristMaximumAngle() {
-        return 90;
+    public DeviceInfo getAutonomousLight() {
+        return new DeviceInfo(100, false);
     }
     
+    @Override
+    public DeviceInfo getClimbDeployMaster() {
+        return new DeviceInfo(50, true);
+    }
+    
+    @Override
+    public DeviceInfo getClimbLeanMaster() {
+        return new DeviceInfo(55, true);
+    }
+    
+    @Override
+    public DeviceInfo getClimbMaster() {
+        return new DeviceInfo(33, true);
+    }
+
+    public DeviceInfo getElevatorEncoder() {
+        return new DeviceInfo(0, true);
+    }
+
+    @Override
+    public DeviceInfo getElevatorFollower() {
+        return new DeviceInfo(22, false);
+    }
+    
+    @Override
+    public DeviceInfo getElevatorLowerLimit() {
+        return new DeviceInfo(1, true);
+    }
+    @Override
+    public DeviceInfo getElevatorMaster() {
+        return new DeviceInfo(25, true);
+    }
+
     @Override
     public DeviceInfo getElevatorUpperLimit() {
         return new DeviceInfo(0, true);
     }
 
     @Override
-    public DeviceInfo getElevatorLowerLimit() {
-        return new DeviceInfo(1, true);
-    }
-
-    @Override
     public DeviceInfo getLeftCollectorMaster() {
         return new DeviceInfo(24, true);
-    }
-    
-    @Override
-    public DeviceInfo getRightCollectorMaster() {
-        return new DeviceInfo(31, false);
-    }
-    @Override
-    public DeviceInfo getLeftDriveMaster() {
-        return new DeviceInfo(34, true);
     }
 
     @Override
@@ -86,13 +96,8 @@ public class Competition2018Robot extends ElectricalContract2018 {
     }
 
     @Override
-    public DeviceInfo getRightDriveMaster() {
-        return new DeviceInfo(21, false);
-    }
-
-    @Override
-    public DeviceInfo getRightDriveFollower() {
-        return new DeviceInfo(20, false);
+    public DeviceInfo getLeftDriveMaster() {
+        return new DeviceInfo(34, true);
     }
 
     @Override
@@ -101,23 +106,38 @@ public class Competition2018Robot extends ElectricalContract2018 {
     }
 
     @Override
+    public DeviceInfo getPawlSolenoidA() {
+        return new DeviceInfo(1, true);
+    }
+
+    @Override
+    public DeviceInfo getPawlSolenoidB() {
+        return new DeviceInfo(2, true);
+    }
+
+    @Override
+    public DeviceInfo getRightCollectorMaster() {
+        return new DeviceInfo(31, false);
+    }
+
+    @Override
+    public DeviceInfo getRightDriveFollower() {
+        return new DeviceInfo(20, false);
+    }
+
+    @Override
+    public DeviceInfo getRightDriveMaster() {
+        return new DeviceInfo(21, false);
+    }
+
+    @Override
     public DeviceInfo getRightDriveMasterEncoder() {
         return new DeviceInfo(0, false);
     }
 
     @Override
-    public boolean elevatorReady() {
-        return true;
-    }
-
-    @Override
-    public boolean wristReady() {
-        return true;
-    }
-
-    @Override
-    public DeviceInfo getWristMaster() {
-        return new DeviceInfo(30, false);
+    public DeviceInfo getShifterSolenoid() {
+        return new DeviceInfo(0, false);
     }
 
     @Override
@@ -126,48 +146,23 @@ public class Competition2018Robot extends ElectricalContract2018 {
     }
 
     @Override
-    public DeviceInfo getWristUpperLimit() {
-        return new DeviceInfo(2, true);
-    }
-
-    @Override
     public DeviceInfo getWristLowerLimit() {
         return new DeviceInfo(3, true);
     }
 
     @Override
-    public boolean collectorReady() {
-        return true;
+    public DeviceInfo getWristMaster() {
+        return new DeviceInfo(30, false);
     }
 
     @Override
-    public DeviceInfo getClimbLeanMaster() {
-        return new DeviceInfo(55, true);
+    public double getWristMaximumAngle() {
+        return 90;
     }
 
     @Override
-    public DeviceInfo getClimbDeployMaster() {
-        return new DeviceInfo(50, true);
-    }
-
-    @Override
-    public DeviceInfo getClimbMaster() {
-        return new DeviceInfo(33, true);
-    }
-
-    @Override
-    public boolean elevatorLowerLimitReady() {
-        return true;
-    }
-
-    @Override
-    public boolean elevatorUpperLimitReady() {
-        return true;
-    }
-
-    @Override
-    public boolean elevatorUsesTalonLimits() {
-        return false;
+    public DeviceInfo getWristUpperLimit() {
+        return new DeviceInfo(2, true);
     }
 
     @Override
@@ -176,7 +171,12 @@ public class Competition2018Robot extends ElectricalContract2018 {
     }
 
     @Override
-    public DeviceInfo getAutonomousLight() {
-        return new DeviceInfo(100, false);
+    public boolean isWristLimitsReady() {
+        return false;
+    }
+
+    @Override
+    public boolean wristReady() {
+        return true;
     }
 }
