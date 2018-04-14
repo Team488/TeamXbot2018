6d30cc7e7277d15d671fef34db5f58fcc464397c
Mr-Recalculate <peregrineautumn@gmail.com>
2018-04-14 11:42:24 -0700

    deleted useless climbing code, but left winch code intact. (#183)
    * deleted useless climbing code, but left winch code intact.
    
    * re-added "AscendSlowlyCommand".
    
    * pressed the format button
    
    * Somehow had old code

--- Competition2018/src/main/java/competition/operator_interface/OperatorCommandMap.java
+++ Competition2018/src/main/java/competition/operator_interface/OperatorCommandMap.java
@@ -4,7 +4,6 @@ import com.google.inject.Inject;
 import com.google.inject.Singleton;
 
 import competition.commandgroups.CollectCubeCommandGroup;
-import competition.commandgroups.MultiCubeNearScaleCommandGroup;
 import competition.commandgroups.ScoreOnSwitchCommandGroup;
 import competition.subsystems.autonomous.AutonomousCommandSupplier.AutonomousMetaprogram;
 import competition.subsystems.autonomous.AutonomousPathSupplier.StartingLocations;
@@ -91,7 +90,7 @@ public class OperatorCommandMap {
         selectSwitch.setMetaprogram(AutonomousMetaprogram.Switch);
         crossLine.setMetaprogram(AutonomousMetaprogram.CrossLine);
         doNothing.setMetaprogram(AutonomousMetaprogram.DoNothing);
-        
+
         addAutoDelay.setDelayChangeAmount(1);
         subtractAutoDelay.setDelayChangeAmount(-1);
 
@@ -109,7 +108,7 @@ public class OperatorCommandMap {
         oi.programmerGamepad.getifAvailable(2).whenPressed(selectScale);
         oi.programmerGamepad.getifAvailable(3).whenPressed(crossLine);
         oi.programmerGamepad.getifAvailable(4).whenPressed(doNothing);
-        
+
         oi.programmerGamepad.getifAvailable(6).whenPressed(advancedScale);
     }
 
@@ -128,110 +127,60 @@ public class OperatorCommandMap {
 
         oi.driverGamepad.getPovIfAvailable(0).whenPressed(velocity);
         oi.driverGamepad.getPovIfAvailable(180).whenPressed(arcade);
-        
+
         pretendMulticube.addPoint(new TotalRobotPoint(
-                new RabbitPoint(
-                        new FieldPose(new XYPair(0, -3*12), new ContiguousHeading(90)), 
-                        PointType.PositionAndHeading, 
-                        PointTerminatingType.Continue, 
-                        PointDriveStyle.Macro),
-                Gear.LOW_GEAR,
-                80));
-        
+                new RabbitPoint(new FieldPose(new XYPair(0, -3 * 12), new ContiguousHeading(90)),
+                        PointType.PositionAndHeading, PointTerminatingType.Continue, PointDriveStyle.Macro),
+                Gear.LOW_GEAR, 80));
+
+        pretendMulticube
+                .addPoint(new TotalRobotPoint(
+                        new RabbitPoint(new FieldPose(new XYPair(0, 0), new ContiguousHeading(225)),
+                                PointType.HeadingOnly, PointTerminatingType.Continue, PointDriveStyle.Macro),
+                        Gear.LOW_GEAR, 80));
+
         pretendMulticube.addPoint(new TotalRobotPoint(
-                new RabbitPoint(
-                        new FieldPose(new XYPair(0, 0), new ContiguousHeading(225)), 
-                        PointType.HeadingOnly, 
-                        PointTerminatingType.Continue, 
-                        PointDriveStyle.Macro),
-                Gear.LOW_GEAR,
-                80));
-        
+                new RabbitPoint(new FieldPose(new XYPair(-3 * 12, -6 * 12), new ContiguousHeading(225)),
+                        PointType.PositionAndHeading, PointTerminatingType.Continue, PointDriveStyle.Micro),
+                Gear.LOW_GEAR, 40));
+
         pretendMulticube.addPoint(new TotalRobotPoint(
-                new RabbitPoint(
-                        new FieldPose(new XYPair(-3*12, -6*12), new ContiguousHeading(225)), 
-                        PointType.PositionAndHeading, 
-                        PointTerminatingType.Continue, 
-                        PointDriveStyle.Micro),
-                Gear.LOW_GEAR,
-                40));
-        
+                new RabbitPoint(new FieldPose(new XYPair(0 * 12, -6 * 12), new ContiguousHeading(180)),
+                        PointType.PositionAndHeading, PointTerminatingType.Continue, PointDriveStyle.Macro),
+                Gear.LOW_GEAR, 80));
+
         pretendMulticube.addPoint(new TotalRobotPoint(
-                new RabbitPoint(
-                        new FieldPose(new XYPair(0*12, -6*12), new ContiguousHeading(180)), 
-                        PointType.PositionAndHeading, 
-                        PointTerminatingType.Continue, 
-                        PointDriveStyle.Macro),
-                Gear.LOW_GEAR,
-                80));
-        
+                new RabbitPoint(new FieldPose(new XYPair(0 * 12, 0 * 12), new ContiguousHeading(90)),
+                        PointType.HeadingOnly, PointTerminatingType.Continue, PointDriveStyle.Macro),
+                Gear.LOW_GEAR, 80));
+
         pretendMulticube.addPoint(new TotalRobotPoint(
-                new RabbitPoint(
-                        new FieldPose(new XYPair(0*12, 0*12), new ContiguousHeading(90)), 
-                        PointType.HeadingOnly, 
-                        PointTerminatingType.Continue, 
-                        PointDriveStyle.Macro),
-                Gear.LOW_GEAR,
-                80));
-        
-        pretendMulticube.addPoint(new TotalRobotPoint(
-                new RabbitPoint(
-                        new FieldPose(new XYPair(0*12, 0*12), new ContiguousHeading(90)), 
-                        PointType.PositionAndHeading, 
-                        PointTerminatingType.Stop, 
-                        PointDriveStyle.Macro),
-                Gear.LOW_GEAR,
-                80));
-        
+                new RabbitPoint(new FieldPose(new XYPair(0 * 12, 0 * 12), new ContiguousHeading(90)),
+                        PointType.PositionAndHeading, PointTerminatingType.Stop, PointDriveStyle.Macro),
+                Gear.LOW_GEAR, 80));
+
         pretendMulticube.includeOnSmartDashboard("A Pretent Multicube");
-        
-        
-            
-        square.addPoint(new RabbitPoint(
-                new FieldPose(new XYPair(0*12, 6*12), new ContiguousHeading(90)),
-                PointType.PositionAndHeading,
-                PointTerminatingType.Stop,
-                PointDriveStyle.Macro));
-        square.addPoint(new RabbitPoint(
-                new FieldPose(new XYPair(0*12, 0*12), new ContiguousHeading(0)),
-                PointType.HeadingOnly,
-                PointTerminatingType.Stop,
-                PointDriveStyle.Macro));
-        
-        square.addPoint(new RabbitPoint(
-                new FieldPose(new XYPair(6*12, 6*12), new ContiguousHeading(0)),
-                PointType.PositionAndHeading,
-                PointTerminatingType.Stop,
-                PointDriveStyle.Macro));
-        square.addPoint(new RabbitPoint(
-                new FieldPose(new XYPair(0*12, 0*12), new ContiguousHeading(-90)),
-                PointType.HeadingOnly,
-                PointTerminatingType.Stop,
-                PointDriveStyle.Macro));
-        
-        square.addPoint(new RabbitPoint(
-                new FieldPose(new XYPair(6*12, 0*12), new ContiguousHeading(-90)),
-                PointType.PositionAndHeading,
-                PointTerminatingType.Stop,
-                PointDriveStyle.Macro));
-        square.addPoint(new RabbitPoint(
-                new FieldPose(new XYPair(0*12, 0*12), new ContiguousHeading(180)),
-                PointType.HeadingOnly,
-                PointTerminatingType.Stop,
-                PointDriveStyle.Macro));
-        
-        square.addPoint(new RabbitPoint(
-                new FieldPose(new XYPair(0*12, 0*12), new ContiguousHeading(180)),
-                PointType.PositionAndHeading,
-                PointTerminatingType.Stop,
-                PointDriveStyle.Macro));
-        square.addPoint(new RabbitPoint(
-                new FieldPose(new XYPair(0*12, 0*12), new ContiguousHeading(90)),
-                PointType.HeadingOnly,
-                PointTerminatingType.Stop,
-                PointDriveStyle.Macro));
-        
-        
+
+        square.addPoint(new RabbitPoint(new FieldPose(new XYPair(0 * 12, 6 * 12), new ContiguousHeading(90)),
+                PointType.PositionAndHeading, PointTerminatingType.Stop, PointDriveStyle.Macro));
+        square.addPoint(new RabbitPoint(new FieldPose(new XYPair(0 * 12, 0 * 12), new ContiguousHeading(0)),
+                PointType.HeadingOnly, PointTerminatingType.Stop, PointDriveStyle.Macro));
+
+        square.addPoint(new RabbitPoint(new FieldPose(new XYPair(6 * 12, 6 * 12), new ContiguousHeading(0)),
+                PointType.PositionAndHeading, PointTerminatingType.Stop, PointDriveStyle.Macro));
+        square.addPoint(new RabbitPoint(new FieldPose(new XYPair(0 * 12, 0 * 12), new ContiguousHeading(-90)),
+                PointType.HeadingOnly, PointTerminatingType.Stop, PointDriveStyle.Macro));
+
+        square.addPoint(new RabbitPoint(new FieldPose(new XYPair(6 * 12, 0 * 12), new ContiguousHeading(-90)),
+                PointType.PositionAndHeading, PointTerminatingType.Stop, PointDriveStyle.Macro));
+        square.addPoint(new RabbitPoint(new FieldPose(new XYPair(0 * 12, 0 * 12), new ContiguousHeading(180)),
+                PointType.HeadingOnly, PointTerminatingType.Stop, PointDriveStyle.Macro));
+
+        square.addPoint(new RabbitPoint(new FieldPose(new XYPair(0 * 12, 0 * 12), new ContiguousHeading(180)),
+                PointType.PositionAndHeading, PointTerminatingType.Stop, PointDriveStyle.Macro));
+        square.addPoint(new RabbitPoint(new FieldPose(new XYPair(0 * 12, 0 * 12), new ContiguousHeading(90)),
+                PointType.HeadingOnly, PointTerminatingType.Stop, PointDriveStyle.Macro));
+
         square.includeOnSmartDashboard("A Square Dancing Robot");
     }
 
@@ -243,9 +192,9 @@ public class OperatorCommandMap {
     }
 
     @Inject
-    public void setupGripperCommands(OperatorInterface oi, GripperEjectCommand eject,
-            GripperIntakeCommand intake, GripperDropCubeCommand dropCube) {
-        
+    public void setupGripperCommands(OperatorInterface oi, GripperEjectCommand eject, GripperIntakeCommand intake,
+            GripperDropCubeCommand dropCube) {
+
         oi.operatorGamepad.getifAvailable(8).whileHeld(dropCube);
 
         oi.driverGamepad.getAnalogIfAvailable(oi.driverLeftTrigger).whileHeld(eject);
@@ -261,11 +210,10 @@ public class OperatorCommandMap {
             EnableElevatorCurrentLimitCommand enableCurrentLimit,
             DisableElevatorCurrentLimitCommand disableCurrentLimit, ExperimentMotionMagicCommand mm,
             ControlElevatorViaJoystickCommand joysticks, ElevatorVelocityCommand velocity,
-            ElevatorDangerousOverrideCommand dangerousOverride,
-            ElevatorSubsystem elevatorSubsystem) {
-        
-        //calibrateElevatorTicks.includeOnSmartDashboard();
-        
+            ElevatorDangerousOverrideCommand dangerousOverride, ElevatorSubsystem elevatorSubsystem) {
+
+        // calibrateElevatorTicks.includeOnSmartDashboard();
+
         oi.operatorGamepad.getifAvailable(6).whileHeld(dangerousOverride);
         oi.operatorGamepad.getifAvailable(7).whenPressed(velocity);
 
@@ -275,7 +223,6 @@ public class OperatorCommandMap {
         targetScaleHighHeight.setGoalHeight(elevatorSubsystem.getTargetScaleHighHeight());
         targetPortalHeight.setGoalHeight(elevatorSubsystem.getTargetExchangeZonePickUpHeight());
 
-        
         oi.operatorGamepad.getifAvailable(1).whenPressed(targetPickUpHeight);
         oi.operatorGamepad.getifAvailable(2).whenPressed(targetPortalHeight);
         oi.operatorGamepad.getifAvailable(3).whenPressed(targetSwitchDropHeight);
@@ -291,7 +238,8 @@ public class OperatorCommandMap {
     }
 
     @Inject
-    public void setupClimberCommands(OperatorInterface oi, AscendClimberCommand ascend, AscendLowPowerCommand ascendSlowly) {
+    public void setupClimberCommands(OperatorInterface oi, AscendClimberCommand ascend,
+            AscendLowPowerCommand ascendSlowly) {
         oi.driverGamepad.getifAvailable(1).whileHeld(ascend); // a
         oi.driverGamepad.getifAvailable(2).whileHeld(ascendSlowly); // b
     }
@@ -302,15 +250,11 @@ public class OperatorCommandMap {
     }
 
     @Inject
-    public void setupVisionCommands(
-                OperatorInterface oi,
-                AcquireVisibleCubeCommand acquireCube,
-                NavToTestGoalCommand testNav,
-                DriveAtVelocityInfinitelyCommand driveAtVelLow,
-                DriveAtVelocityInfinitelyCommand driveAtVelHigh,
-                IdentifyAndPurePursuitToVisibleCubeCommand driveToLocalCubeCommand,
-                ExtendRetractZedCommand extendZed,
-                ExtendRetractZedCommand retractZed) {
+    public void setupVisionCommands(OperatorInterface oi, AcquireVisibleCubeCommand acquireCube,
+            NavToTestGoalCommand testNav, DriveAtVelocityInfinitelyCommand driveAtVelLow,
+            DriveAtVelocityInfinitelyCommand driveAtVelHigh,
+            IdentifyAndPurePursuitToVisibleCubeCommand driveToLocalCubeCommand, ExtendRetractZedCommand extendZed,
+            ExtendRetractZedCommand retractZed) {
         acquireCube.includeOnSmartDashboard();
         testNav.includeOnSmartDashboard();
 
@@ -338,7 +282,7 @@ public class OperatorCommandMap {
             SetWristAngleCommand medium, SetWristAngleCommand high, WristDangerousOverrideCommand danger) {
         oi.operatorGamepad.getifAvailable(9).whenPressed(calibrate);
         loseCalibration.includeOnSmartDashboard();
-        
+
         oi.operatorGamepad.getifAvailable(5).whileHeld(danger);
 
         low.setGoalAngle(0);
