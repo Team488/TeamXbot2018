c1b03873a3adb533d5c7bc244eb9ec971c5fca5b
WasabiFan <wasabifan@outlook.com>
2018-04-15 00:26:59 -0700

    Update thruster I/O to use hobby control protocol (proper pulse width) rather than duty cycle

--- Competition2018/src/main/java/competition/subsystems/fhds/FlyingHookDeliverySensorSubsystem.java
+++ Competition2018/src/main/java/competition/subsystems/fhds/FlyingHookDeliverySensorSubsystem.java
@@ -9,8 +9,10 @@ import competition.subsystems.offboard.OffboardFramePackingUtils;
 import competition.subsystems.offboard.XOffboardCommsInterface;
 import competition.subsystems.offboard.packets.DroneMotorCommandPacket;
 import xbot.common.command.BaseSubsystem;
+import xbot.common.controls.actuators.XDigitalOutput;
 import xbot.common.controls.actuators.XPWM;
 import xbot.common.injection.wpi_factories.CommonLibFactory;
+import xbot.common.math.MathUtils;
 import xbot.common.math.XYPair;
 
 @Singleton
@@ -26,18 +28,57 @@ public class FlyingHookDeliverySensorSubsystem extends BaseSubsystem {
     private volatile boolean isDroneRunning = false;
     private volatile Object droneThreadLock = new Object();
     
+    private static class FHDSMotorOutput {
+        // Maximum frequency is (1000 (ms/s) / 2 ms) = 500Hz, but we need to
+        // ensure there is margin between each pulse so they are individually
+        // discernible. Division factor is chosen arbitrarily.
+        private final double FREQUENCY = (1000d/2d) / 1.5;
+        private final double PERIOD = 1/FREQUENCY;
+        
+        XDigitalOutput output;
+        public FHDSMotorOutput(XDigitalOutput output) {
+            this.output = output;
+        }
+        
+        private double calculateDutyCycle(double throttle) {
+            double targetTime = MathUtils.scaleDouble(throttle, -1, 1, 1.0, 2.0) / 1000;
+            return targetTime / PERIOD;
+        }
+        
+        public void start(double throttle) {
+            output.setPWMRate(FREQUENCY);
+            output.enablePWM(calculateDutyCycle(throttle));
+        }
+        
+        public void set(double throttle) {
+            output.updateDutyCycle(calculateDutyCycle(throttle));
+        }
+        
+        public void stop() {
+            output.disablePWM();
+        }
+    }
+    
     @Inject
     public FlyingHookDeliverySensorSubsystem(XOffboardCommsInterface comms, CommonLibFactory clf) {
         
         fhdsThread = new Thread(() -> {
-            XPWM leftFrontMotor = clf.createPWM(1);
-            XPWM rightFrontMotor = clf.createPWM(2);
-            XPWM leftRearMotor = clf.createPWM(3);
-            XPWM rightRearMotor = clf.createPWM(4);
+            FHDSMotorOutput leftFrontMotor = new FHDSMotorOutput(clf.createDigitalOutput(1));
+            FHDSMotorOutput rightFrontMotor = new FHDSMotorOutput(clf.createDigitalOutput(2));
+            FHDSMotorOutput leftRearMotor = new FHDSMotorOutput(clf.createDigitalOutput(3));
+            FHDSMotorOutput rightRearMotor = new FHDSMotorOutput(clf.createDigitalOutput(4));
             
+            boolean isPwmStarted = false;
             while (!Thread.interrupted()) {
                 if (!isDroneRunning) {
                     try {
+                        log.info("Stopping all FHDS PWMs");
+                        isPwmStarted = false;
+                        leftFrontMotor.stop();
+                        rightFrontMotor.stop();
+                        leftRearMotor.stop();
+                        rightRearMotor.stop();
+                        
                         log.info("Drone thread waiting for continuation");
                         droneThreadLock.wait();
                         log.info("Drone thread resumed after wait");
@@ -51,11 +92,21 @@ public class FlyingHookDeliverySensorSubsystem extends BaseSubsystem {
                 if (packet != null) {
                    if (packet.packetType == OffboardCommsConstants.PACKET_TYPE_DRONE_MOTOR_POWER) {
                        DroneMotorCommandPacket commandPacket = DroneMotorCommandPacket.parse(packet.data);
-
-                       leftFrontMotor.setSigned(commandPacket.leftFront);
-                       rightFrontMotor.setSigned(commandPacket.rightFront);
-                       leftRearMotor.setSigned(commandPacket.leftRear);
-                       rightRearMotor.setSigned(commandPacket.rightRear);
+                       
+                       if (isPwmStarted) {
+                           leftFrontMotor.set(commandPacket.leftFront);
+                           rightFrontMotor.set(commandPacket.rightFront);
+                           leftRearMotor.set(commandPacket.leftRear);
+                           rightRearMotor.set(commandPacket.rightRear);
+                       }
+                       else {
+                           log.info("Starting all FHDS PWMs");
+                           isPwmStarted = true;
+                           leftFrontMotor.start(commandPacket.leftFront);
+                           rightFrontMotor.start(commandPacket.rightFront);
+                           leftRearMotor.start(commandPacket.leftRear);
+                           rightRearMotor.start(commandPacket.rightRear);
+                       }
                    }
                    else {
                        log.warn("Received unknown packet in FHDS loop");
